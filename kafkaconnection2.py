853e224235558f22a97ef1946fa546c77817bb2d
SaraAdr <sara_adr@hotmail.com>
2024-04-04 11:22:04 +0200

    Prueba kafka connection 1

--- kafkaconnection2.py
+++ kafkaconnection2.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.providers.apache.kafka.operators.consume import ConsumeFromTopicOperator
 from datetime import datetime
+from airflow.operators.bash_operator import BashOperator
 
 default_args = {
     'owner': 'sadr',
@@ -14,9 +15,22 @@ with DAG(
     schedule_interval='@daily'
     ) as dag:
 
-    consume_and_analyze_data = ConsumeFromTopicOperator(
-        task_id='consume_and_analyze_data',
-        topic='test1',
-        bootstrap_servers='kafka_broker:9092',
-        group_id='1',
+    t2 = ConsumeFromTopicOperator(
+        kafka_config_id="kafka_connection",
+        task_id="Consume topic test1 kafka",
+        topics=["test1"],
+        #apply_function="example_dag_hello_kafka.consumer_function",
+        #apply_function_kwargs={"prefix": "consumed:::"},
+        commit_cadence="end_of_batch",
+        max_messages=1,
+        max_batch_size=5,
     )
+
+ # Mostrar el mensaje consumido en la consola
+    print_message = BashOperator(
+        task_id='print_message',
+        bash_command="echo '{{ ti.xcom_pull(task_ids=\"consume_from_kafka\") }}'"
+    )
+
+    # Establecer la secuencia de tareas
+    t2 >> print_message
