ff8dcece0765fe1347a8241043863cc3aad30859
SaraAdr <sara_adr@hotmail.com>
2024-04-06 23:41:31 +0200

    dag3

--- kafka3.py
+++ kafka3.py
@@ -1,48 +1,21 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
-from airflow.providers.apache.kafka.sensors import KafkaSensor
-from airflow.providers.apache.kafka.operators import KafkaConsumeOperator
+from airflow.operators.kafka import KafkaOperator
 from datetime import datetime
 
-# Parámetros del DAG
-topic_name = "test1"
-bootstrap_servers = ["10.96.45.152:9092"]
+dag = DAG(
+    dag_id="mi_dag_kafka_3",
+    schedule_interval="@once",
+    start_date=datetime(2024, 4, 6),
+)
 
-# DAG
-with DAG(
-    dag_id="kafka_consume_bash",
-    schedule_interval="* * * * *",
-    start_date=datetime.now(),
-    catchup=False,
-) as dag:
-    
-    
-    # Tarea que imprime un mensaje al iniciar
-    start_message = BashOperator(
-        task_id="start_message",
-        bash_command="echo 'Iniciando DAG'",
-    )
+# Crear la tarea para consumir mensajes
+consume_task = KafkaOperator(
+    task_id="consumir_mensajes_kafka",
+    topic="test1",
+    bootstrap_servers="10.96.45.152:9092",
+    group_id="1",
+    dag=dag,
+)
 
-    # Sensor que espera un mensaje en el topic
-    kafka_sensor = KafkaSensor(
-        task_id="wait_for_message",
-        topic=topic_name,
-        bootstrap_servers=bootstrap_servers,
-    )
-
-    # Operador que consume el mensaje y lo muestra por Bash
-    consume_and_print_message = KafkaConsumeOperator(
-        task_id="consume_and_print_message",
-        topic=topic_name,
-        bootstrap_servers=bootstrap_servers,
-        bash_command="echo {{ ti.message }}",
-    )
-
-    # Tarea que imprime un mensaje al finalizar
-    end_message = BashOperator(
-        task_id="end_message",
-        bash_command="echo 'DAG finalizado'",
-    )
-
-    # Flujo del DAG
-    start_message >> kafka_sensor >> consume_and_print_message >> end_message
+# Iniciar el DAG
+dag.run()
