aeab693612da6854186e37654d61934c828f8968
SaraAdr <sara_adr@hotmail.com>
2024-04-06 20:55:30 +0200

    Prueba de funcionamiento de daily

--- kafkaconnection2.py
+++ kafkaconnection2.py
@@ -6,13 +6,13 @@ from airflow.operators.bash_operator import BashOperator
 default_args = {
     'owner': 'sadr',
     'depends_on_past': False,
-    'start_date': datetime(2023, 12, 1),
+    'start_date': datetime(2024, 4, 4),
 }
 
 with DAG(
     'kafka_airflow_integration', 
     default_args=default_args,
-    schedule_interval='@daily'
+    schedule_interval='@daily',
     ) as dag:
 
     t2 = ConsumeFromTopicOperator(
