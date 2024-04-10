bd30370135e90a2f3532de3d0f16a30f89234496
SaraAdr <sara_adr@hotmail.com>
2024-04-10 10:05:58 +0200

    vientos parte 1

---
+++ vientos.py
@@ -0,0 +1,65 @@
+from airflow.providers.mongo.hooks.mongo import MongoHook
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+import json
+
+default_args = {
+    "owner": "Sadr",
+    "depend_on_past": False,
+    "start_date": datetime(2024, 4, 4),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=2),
+}
+
+def generate_json():
+    # Datos de ejemplo
+    data = {
+        "coordenadas": {
+            "x": 10.01,
+            "y": 14.34 
+        },
+        "inicio_periodo": "14:05:02 10-05-2024",
+        "fin_periodo": "16:00:00 12-05-2024",
+        "extension_tiff": "?",
+        "fichero_tiff": False,
+        "horas_periodo": 1
+    }
+    json_data = json.dumps(data)
+    return json_data
+
+def save_to_mongodb(json_data, **kwargs):
+    # Establecer conexión con MongoDB
+    hook = MongoHook(mongo_conn_id='mongoid')
+    client = hook.get_conn()
+    db = client['airflow']
+    collection = db['datalake']
+    # Insertar el JSON en la colección
+    collection.insert_one(json.loads(json_data))
+    print(f"Connected to MongoDB - {client.server_info()}")
+
+dag = DAG(
+    'json_to_mongodb',
+    default_args=default_args,
+    description='Genera un JSON y lo guarda en MongoDB',
+    schedule_interval='@once',
+)
+
+# Define los operadores en el DAG
+generate_json_task = PythonOperator(
+    task_id='generate_json',
+    python_callable=generate_json,
+    dag=dag,
+)
+
+save_to_mongodb_task = PythonOperator(
+    task_id='save_to_mongodb',
+    python_callable=save_to_mongodb,
+    op_kwargs={'json_data': generate_json()},  # Pasa el resultado de generate_json como argumento
+    dag=dag,
+)
+
+# Establece la secuencia de tareas
+generate_json_task >> save_to_mongodb_task
