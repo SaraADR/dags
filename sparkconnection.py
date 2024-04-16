95ac0a0cac2b07ca46d3a56e2cc7c2abfa2737a4
SaraAdr <sara_adr@hotmail.com>
2024-04-16 08:58:57 +0200

    ejecucion spark

--- sparkconnection.py
+++ sparkconnection.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.apache.spark.operators import SparkSubmitOperator
+
 
 default_args = {
     "owner": "Sadr",
@@ -25,9 +27,20 @@ with DAG(
     tags=["Spark"],
 ) as dag:
 
-    spark_task = PythonOperator(
-        task_id='spark_id',
-        python_callable=execute_spark_script,
-        op_kwargs={'spark_script_path': '/opt/airflow/dags/repo/archivos/spark1.py'},
-        dag=dag,
+    # spark_task = PythonOperator(
+    #     task_id='spark_id',
+    #     python_callable=execute_spark_script,
+    #     op_kwargs={'spark_script_path': '/opt/airflow/dags/repo/archivos/spark1.py'},
+    #     dag=dag,
+    # )
+
+    task_elt_documento_pagar = SparkSubmitOperator(
+    task_id='elt_documento_pagar_spark',
+    conn_id='spark_id',
+    application="/opt/airflow/dags/repo/archivos/spark1.py",
+    application_args=[],#caso precise enviar dados da dag para o job airflow utilize esta propriedade
+    total_executor_cores=2,
+    executor_memory="30g",
+    conf={},
+    packages=""
     )
