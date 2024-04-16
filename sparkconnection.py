9e5a88b2abbf92cfd54801bfb7d84066bd387293
SaraAdr <sara_adr@hotmail.com>
2024-04-16 11:33:30 +0200

    spark_submit

--- sparkconnection.py
+++ sparkconnection.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.providers.apache.spark.operators import SparkSubmitOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
 
 default_args = {
