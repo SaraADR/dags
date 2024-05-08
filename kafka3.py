ef8e27557a3b73d506c555c60ef39a9f2feab6ba
SaraAdr <sara_adr@hotmail.com>
2024-05-08 09:22:14 +0200

    jsonload

--- kafka3.py
+++ kafka3.py
@@ -24,7 +24,7 @@ consumer_logger = logging.getLogger("airflow")
 def consumer_function(message, prefix=None):
     message_json = json.loads(message.value().decode('utf-8'))
     consumer_logger.info(f"{prefix}  {message_json}")
-    return message_json
+    return message.value().decode('utf-8')
 
 def buscar_registro(message_json  , **kwargs):
     engine = create_engine(database_url)
