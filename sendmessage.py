0aa3dfab236c041ff94e4e789788fc45ed6db366
SaraAdr <sara_adr@hotmail.com>
2024-06-24 02:14:46 +0200

    .2

--- sendmessage.py
+++ sendmessage.py
@@ -1,40 +1,29 @@
+from datetime import datetime
 from airflow import DAG
 from airflow.operators.email import EmailOperator
-from airflow.utils.dates import days_ago
-from airflow.models import Variable
 
 default_args = {
     'owner': 'airflow',
-    'start_date': days_ago(1),
+    'start_date': datetime(2023, 1, 1),
     'email_on_failure': False,
     'email_on_retry': False,
 }
 
-dag = DAG(
-    'email_notification_dag',
+with DAG(
+    'send_test_email',
     default_args=default_args,
-    description='Email notification',
+    description='A simple DAG to send a test email',
     schedule_interval=None,
-)
+    catchup=False,
+) as dag:
 
-def create_email_task(dag, to, subject, body, cc, bcc):
-    return EmailOperator(
+    send_email = EmailOperator(
         task_id='send_email',
-        to=to,
-        subject=subject,
-        html_content=body,
-        cc=cc,
-        bcc=bcc,
-        #files=attachments,
-        dag=dag,
+        to='sara_adr@hotmail.com',  # Cambia esto a la dirección de correo del destinatario
+        subject='Test Email from Airflow',
+        html_content='<p>This is a test email sent from an Airflow DAG!</p>',
     )
 
-# Valores de ejemplo; reemplazar con los valores reales
-to = 'sara.arrdr@gmail.com'
-subject = 'Test Email'
-body = 'This is a test email'
-cc = ['sara_adr@hotmail.com']
-bcc = ['sara.arrdr@gmail.com']
-#attachments = ['/path/to/attachment.txt']
+    send_email
 
-send_email_task = create_email_task(dag, to, subject, body, cc, bcc)
+#stcp fpia fdtb etek
