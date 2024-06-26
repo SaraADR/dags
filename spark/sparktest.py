d243819bcb31fe384d66f51112b4679ea6f5d3ff
SaraAdr <sara_adr@hotmail.com>
2024-06-26 19:59:56 +0200

    spark ejemplo

--- spark/sparktest.py
+++ spark/sparktest.py
@@ -1,14 +1,18 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder \
-    .appName("Example PySpark Job") \
-    .getOrCreate()
+def show_text(spark, text, times):
+    # Crea un RDD con el texto repetido la cantidad de veces especificada
+    rdd = spark.sparkContext.parallelize([text] * times)
+    
+    # Muestra el texto por consola
+    rdd.foreach(lambda x: print(x))
 
-# Tu código PySpark aquí
-data = [("Java", 20000), ("Python", 100000), ("Scala", 3000)]
-columns = ["Language", "Users"]
-
-df = spark.createDataFrame(data).toDF(*columns)
-df.show()
-
-spark.stop()
+if __name__ == "__main__":
+    # Inicializa una sesión de Spark
+    spark = SparkSession.builder.appName("ShowTextApp").getOrCreate()
+    
+    # Llama a la función con el texto deseado y la cantidad de veces que debe mostrarse
+    show_text(spark, "Hola, este es el texto que se muestra por consola.", 5)
+    
+    # Detiene la sesión de Spark
+    spark.stop()
