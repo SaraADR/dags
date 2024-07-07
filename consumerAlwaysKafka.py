bce564226253831a5a27f3e4e1e72f498a9ad581
SaraAdr <sara_adr@hotmail.com>
2024-07-07 23:32:42 +0200

    easy

--- consumerAlwaysKafka.py
+++ consumerAlwaysKafka.py
@@ -2,7 +2,7 @@ import json
 from airflow import DAG
 from airflow.providers.apache.kafka.operators.consume import ConsumeFromTopicOperator
 from airflow.operators.python import PythonOperator
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.api.common.experimental.trigger_dag import trigger_dag
 from datetime import datetime, timedelta
 
 def handle_message(message, **kwargs):
@@ -12,14 +12,9 @@ def handle_message(message, **kwargs):
         return None
 
     if msg_dict.get('destination') == 'email':
+        trigger_dag(dag_id='sendmessage3', run_id=None, conf=msg_dict)
         return msg_dict
     return None
-
-def prepare_dag_run(context, **kwargs):
-    ti = context['task_instance']
-    message = ti.xcom_pull(task_ids='consume_from_kafka')
-    if message:
-        context['task_instance'].xcom_push(key='dag_run_payload', value=message)
 
 default_args = {
     'owner': 'airflow',
@@ -47,18 +42,4 @@ consume_task = ConsumeFromTopicOperator(
     dag=dag,
 )
 
-prepare_dag_run_task = PythonOperator(
-    task_id='prepare_dag_run',
-    python_callable=prepare_dag_run,
-    provide_context=True,
-    dag=dag,
-)
-
-trigger_dag_task = TriggerDagRunOperator(
-    task_id='trigger_dag_run',
-    trigger_dag_id='sendmessage3',
-    conf="{{ task_instance.xcom_pull(task_ids='prepare_dag_run', key='dag_run_payload') }}",
-    dag=dag,
-)
-
-consume_task >> prepare_dag_run_task >> trigger_dag_task
+consume_task
