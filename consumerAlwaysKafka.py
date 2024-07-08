e7a3e641f07cb905509bb1a183a2e40b1ff3f30f
SaraAdr <sara_adr@hotmail.com>
2024-07-08 19:13:19 +0200

    de locos al rio

--- consumerAlwaysKafka.py
+++ consumerAlwaysKafka.py
@@ -1,9 +1,8 @@
 import json
 from airflow import DAG
+from airflow.operators.python import PythonOperator
 from airflow.providers.apache.kafka.operators.consume import ConsumeFromTopicOperator
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-from airflow.operators.python import PythonOperator, BranchPythonOperator
-from airflow.operators.dummy import DummyOperator
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
 from datetime import datetime, timedelta
 
 def consumer_function(message, prefix, **kwargs):
@@ -13,35 +12,24 @@ def consumer_function(message, prefix, **kwargs):
         if msg_value:
             try:
                 msg_json = json.loads(msg_value)
-                if msg_json.get('destination') == 'email':
+                if msg_json.get('destination') == 'email' and msg_json.get('status') == 'pending':
                     return msg_json
             except json.JSONDecodeError as e:
                 print(f"Error decoding JSON: {e}")
         else:
             print("Empty message received")
-    return None
+    return "{'destination' : 'none'}"
 
-def decide_which_path(**kwargs):
-    # Pull the message JSON from the XCom
-    msg_json = kwargs['task_instance'].xcom_pull(task_ids='consume_from_topic')
-    # Decide the next task based on whether msg_json is None or not
-    if msg_json:
-        return 'trigger_email_handler'
-    else:
-        return 'no_op'
-
-def trigger_email_handler_dag_run(msg_json, **kwargs):
-    # Logic to trigger the DAG
-    if msg_json:
-        print(f"Triggering email handler DAG with config: {msg_json}")
-        trigger = TriggerDagRunOperator(
-            task_id='trigger_email_handler_dag_run_inner',
-            trigger_dag_id='recivekafka',
-            conf=msg_json
-        )
-        trigger.execute(context=kwargs)
-    else:
-        print("No valid message to trigger DAG")
+def trigger_email_handler_dag_run(conf, **kwargs):
+    if json.loads(conf).get('destination') == 'none':
+        return None
+    trigger_dag_run = TriggerDagRunOperator(
+        task_id='trigger_email_handler',
+        trigger_dag_id='recivekafka',
+        conf=conf,
+        dag=kwargs['dag']
+    )
+    trigger_dag_run.execute(context=kwargs)
 
 default_args = {
     'owner': 'airflow',
@@ -68,15 +56,8 @@ consume_from_topic = ConsumeFromTopicOperator(
     apply_function=consumer_function,
     apply_function_kwargs={"prefix": "consumed:::"},
     commit_cadence="end_of_batch",
-    max_messages=1,
-    max_batch_size=1,
-    dag=dag,
-)
-
-decide_path = BranchPythonOperator(
-    task_id='decide_path',
-    python_callable=decide_which_path,
-    provide_context=True,
+    max_messages=10,
+    max_batch_size=2,
     dag=dag,
 )
 
@@ -87,10 +68,4 @@ trigger_email_handler = PythonOperator(
     op_args=["{{ task_instance.xcom_pull(task_ids='consume_from_topic') }}"],
     dag=dag,
 )
-
-no_op = DummyOperator(
-    task_id='no_op',
-    dag=dag,
-)
-
-consume_from_topic >> decide_path >> [trigger_email_handler, no_op]
+consume_from_topic 
