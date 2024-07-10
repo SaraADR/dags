a6dcba28e3f485ad27fb27216f85c12b1e8e26db
SaraAdr <sara_adr@hotmail.com>
2024-07-10 17:26:51 +0200

    plantilla1

--- sendmessageplantilla.py
+++ sendmessageplantilla.py
@@ -9,10 +9,7 @@ import ast
 import os
 from jinja2 import Template
 
-DAGS_FOLDER = os.path.dirname(os.path.realpath(__file__))
-TEMPLATE_PATH = os.path.join(DAGS_FOLDER, 'repo', 'recursos', 'plantillacorreo.html')
-LOGO_PATH = os.path.join(DAGS_FOLDER, 'repo', 'recursos', 'dummy.jpg')
-
+PLANTILLA_1 = './dags/repo/recursos/plantillacorreo.html'
 
 
 default_args = {
@@ -25,56 +22,20 @@ default_args = {
     'retry_delay': timedelta(minutes=1),
 }
 
-def render_template():
+def render_template(message_dict):
 
-    current_dir = os.getcwd()
-    print(f"Current working directory: {current_dir}")
-    
-    # Listar el contenido del directorio actual
-    print("Content of current directory:")
-    print(os.listdir(current_dir + '/dags'))
-    
-    # Verificar y listar el contenido del directorio de recursos
-    recursos_dir = os.path.join(current_dir, 'dags','repo', 'recursos')
-    print(f"Content of recursos directory ({recursos_dir}):")
-    if os.path.exists(recursos_dir):
-        print(os.listdir(recursos_dir))
-    else:
-        print(f"Directory {recursos_dir} does not exist.")
+    data = json.loads(message_dict.get('data', '{}'))
 
-
-
-    # Verificar si los archivos existen
-    print(f"TEMPLATE_PATH: {DAGS_FOLDER}, exists: {os.path.exists(DAGS_FOLDER)}")
-    print(os.listdir(DAGS_FOLDER))
-
-
-    # data = json.loads(message_dict.get('data', '{}'))
-
-    # print(f"Template Path: {TEMPLATE_PATH}")
-    # print(f"Logo path: {LOGO_PATH}")
-
-    with open('./dags/repo/recursos/plantillacorreo.html') as file_:
-        template = Template(file_.read())
-
-    # t = Template('Hello, {{ name }}!')
-    # print(t.render(name='John Doe'))
-
-    with open('./dags/repo/recursos/plantillacorreo.html') as file:
+    with open(PLANTILLA_1) as file:
         template_str = file.read()
     jinja_template = Template(template_str)
 
-    # email_data = {
-    #     'nombre': data.get('to', 'default@example.com'),
-    #     'dato1': data.get('subject', 'No Subject'),
-    #     'dato2': data.get('subject', 'No Subject')
-    # }
-
     email_data = {
-            'nombre': 'default@example.com',
-            'dato1': 'subject',
-            'dato2': 'No Subject'
-        }
+        'nombre': data.get('to', 'default@example.com'),
+        'dato1':  data.get('subject', 'No Subject'),
+        'dato2':  data.get('subject', 'No Subject')
+    }
+
     email_content = jinja_template.render(email_data)
 
     return email_content
@@ -83,36 +44,31 @@ def render_template():
 
 def print_message_and_send_email(**context):
 
-    # message = context['dag_run'].conf
-    # print(f"Received message: {message}")
+    message = context['dag_run'].conf
+    print(f"Received message: {message}")
 
-    
-
-    #message_dict = ast.literal_eval(message['message'])
-    email_body = render_template()
+    message_dict = ast.literal_eval(message['message'])
+    email_body = render_template(message_dict)
     print(email_body)
 
 
-    # context['ti'].xcom_push(key='message_id', value=message_dict.get('id'))
-    # data = json.loads(message_dict.get('data', '{}'))  
-    # print(f"Received message: {data}")
-    # to = data.get('to', 'default@example.com')
-    # subject = data.get('subject', 'No Subject')
-    # body = data.get('body', 'No Body')
+    context['ti'].xcom_push(key='message_id', value=message_dict.get('id'))
+    data = json.loads(message_dict.get('data', '{}'))  
+    print(f"Received message: {data}")
+    to = data.get('to', 'default@example.com')
+    subject = data.get('subject', 'No Subject')
 
-
-
-    # email_operator = EmailOperator(
-    #     task_id='send_email_task',
-    #     to=to,
-    #     subject=subject,
-    #     html_content=f'<p>{email_body}</p>',
-    #     conn_id='smtp_default',
-    #     mime_subtype='related',
-    #     files=LOGO_PATH
-    # )
+    email_operator = EmailOperator(
+        task_id='send_email_task',
+        to=to,
+        subject=subject,
+        html_content=f'<p>{email_body}</p>',
+        conn_id='smtp_default',
+        mime_subtype='related',
+        files=LOGO_PATH
+    )
     
-    # return email_operator.execute(context)
+    return email_operator.execute(context)
 
 
 dag = DAG(
