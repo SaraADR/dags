a1adc5b7f88d3130c1d1ee6855719e8b575916e2
SaraAdr <sara_adr@hotmail.com>
2024-07-12 11:39:48 +0200

    Cambios de nombre y control de datos divididos

---
+++ kafka_consumer_archivos.py
@@ -0,0 +1,103 @@
+import json
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.apache.kafka.operators.consume import ConsumeFromTopicOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python import BranchPythonOperator
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
+from datetime import datetime, timedelta, timezone
+from airflow.models import Variable
+from airflow.exceptions import AirflowSkipException
+import mimetypes
+
+def consumer_function(message, prefix, **kwargs):
+
+    if message is not None:
+
+        mime_type, _ = mimetypes.guess_type(message)
+        if mime_type in ["image/tiff"]:
+            print("Esto es un tiff")
+            Variable.set("my_variable_key", None)
+            return None    
+        elif mime_type in ["image/jpeg"]:
+            print("Esto es un jpg")
+            Variable.set("my_variable_key", None)
+            return None    
+        else:
+            print("Unknown mime type")
+            Variable.set("my_variable_key", None)
+            return None
+        return None  
+    else:
+
+        return None     
+
+
+
+
+def trigger_email_handler(**kwargs):
+    try:
+        value_pulled = Variable.get("my_variable_key")
+    except KeyError:
+        print("Variable my_variable_key does not exist")
+        raise AirflowSkipException("Variable my_variable_key does not exist")
+    
+
+    if value_pulled is not None and value_pulled != 'null':
+        try:
+
+            trigger = TriggerDagRunOperator(
+                task_id='trigger_email_handler_inner',
+                trigger_dag_id='send_email_wplantilla',
+                conf={'message': value_pulled}, 
+                execution_date=datetime.now().replace(tzinfo=timezone.utc),
+                dag=dag,
+            )
+            trigger.execute(context=kwargs)
+            Variable.delete("my_variable_key")
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+    else:
+        print("No message pulled from XCom")
+        Variable.delete("my_variable_key")
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 7, 7),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
+dag = DAG(
+    'kafka_consumer_archivos',
+    default_args=default_args,
+    description='DAG que consume mensajes de Kafka y dispara otro DAG para archivos',
+    schedule_interval='*/3 * * * *',
+    catchup=False
+)
+
+consume_from_topic = ConsumeFromTopicOperator(
+    kafka_config_id="kafka_connection",
+    task_id="consume_from_topic",
+    topics=["archivos"],
+    apply_function=consumer_function,
+    apply_function_kwargs={"prefix": "consumed:::"},
+    commit_cadence="end_of_batch",
+    max_messages=1,
+    max_batch_size=2,
+    dag=dag,
+)
+
+trigger_email_handler_task = PythonOperator(
+    task_id='trigger_email_handler',
+    python_callable=trigger_email_handler,
+    provide_context=True,
+    dag=dag,
+)
+
+
+consume_from_topic >> trigger_email_handler_task
