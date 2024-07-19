2af809780952038670d4291db7fcc339f9d7666d
Óscar <oscar.lopez@cuatrodigital.com>
2024-07-19 08:15:58 +0200

    aa

--- process_coordinates.py
+++ process_coordinates.py
@@ -1,4 +1,5 @@
 import io
+import json
 import tempfile
 import uuid
 from airflow import DAG
@@ -11,13 +12,19 @@ from airflow.hooks.base_hook import BaseHook
 
 def process_kafka_message(**context):
     # Extraer el mensaje del contexto de Airflow
-    message = context['dag_run'].conf
-
+    try:
+        message = context['dag_run'].conf
+    except KeyError:
+        raise KeyError("No configuration found in the DAG run configuration.")
+    
     if not message:
         raise KeyError("No message found in the DAG run configuration.")
     
-    # Si el mensaje no es un JSON, se toma como string directamente
-    message_content = message.get('message', None)
+    try:
+        # Si el mensaje no es un JSON, se toma como string directamente
+        message_content = message.get('message', None)
+    except KeyError:
+        raise ValueError("No valid 'message' key found in the DAG run configuration.")
     
     if not message_content:
         raise ValueError("No valid 'message' key found in the DAG run configuration.")
