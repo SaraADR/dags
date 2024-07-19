c43005a0c5d7e4f7dcfd211ee9665daf7c3c016a
Óscar <oscar.lopez@cuatrodigital.com>
2024-07-19 11:14:50 +0200

    aa

--- kafka_consumer_archivos.py
+++ kafka_consumer_archivos.py
@@ -6,12 +6,14 @@ import zipfile
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.apache.kafka.operators.consume import ConsumeFromTopicOperator
+from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python import BranchPythonOperator
 from airflow.operators.dagrun_operator import TriggerDagRunOperator
 from datetime import datetime, timedelta, timezone
 from airflow.models import Variable
 from airflow.exceptions import AirflowSkipException
 import tempfile
+
 
 def consumer_function(message, prefix, **kwargs):
     print("Esto es el mensaje")
@@ -26,48 +28,50 @@ def consumer_function(message, prefix, **kwargs):
         Variable.set("key", None)        
         Variable.set("value", None)  
 
+
 def choose_branch(**kwargs):
     nombre_fichero = Variable.get("key")
     print(f"{nombre_fichero}")
 
-    file_extension = os.path.splitext(nombre_fichero)[1].strip().lower()
+    file_extension = os.path.splitext(nombre_fichero)[1].strip().lower().replace("'", "")
     print(f"Extensión del archivo: {file_extension}")
 
     if file_extension == '.zip':
         return 'process_zip_task'
-    elif file_extension in ['.tiff', '.tif']:
+    elif file_extension == '.tiff' or file_extension == '.tif':
         return 'process_tiff_task'
-    elif file_extension in ['.jpg', '.jpeg']:
+    elif file_extension == '.jpg' or file_extension == '.jpeg':
         return 'process_jpg_task'
     elif file_extension == '.png':
         return 'process_png_task'
-    elif file_extension in ['.mp4', '.avi', '.mov']:
+    elif file_extension == '.mp4' or file_extension == '.avi' or file_extension == '.mov':
         return 'process_video_task'
     elif file_extension == '.json':
         return 'process_json_task'
+    elif file_extension == 'no_message_task':
+        return 'no_message_task'
     else:
         return 'unknown_or_none_file_task'
+
 
 def process_zip_file(**kwargs):
     try:
         value_pulled = Variable.get("value")
         print("Processing ZIP file")
+        first_40_values = value_pulled[:40]
+        print("First 40 values:", first_40_values)
 
-        if not value_pulled:
-            print("No data to process")
-            raise AirflowSkipException("No data to process")
-
-        # Convert bytes to a proper format for zip processing
-        value_pulled = ast.literal_eval(value_pulled)  # Assumes the value is a string representation of bytes
+        message_dict = ast.literal_eval(value_pulled)
 
         with tempfile.TemporaryDirectory() as temp_dir:
+            temp_unzip_path = os.path.join(temp_dir, 'unzip')
             temp_zip_path = os.path.join(temp_dir, 'zip')
 
-            # Crear el directorio temporal
+            # Crear los subdirectorios temporales
+            os.makedirs(temp_unzip_path, exist_ok=True)
             os.makedirs(temp_zip_path, exist_ok=True)
 
-            # Unzip the content
-            with zipfile.ZipFile(io.BytesIO(value_pulled)) as zip_file:
+            with zipfile.ZipFile(io.BytesIO(message_dict)) as zip_file:
                 # Obtener la lista de archivos dentro del ZIP
                 file_list = zip_file.namelist()
                 print("Archivos en el ZIP:", file_list)
@@ -78,30 +82,29 @@ def process_zip_file(**kwargs):
                 for file_name in file_list:
                     with zip_file.open(file_name) as file:
                         content = file.read()
-                        print(f"Contenido del archivo {file_name}: {content[:10]}...")
+                        print(f"Contenido del archivo {file_name}: {content[:10]}...")  
 
                     if file_name.lower().endswith(('.mp4', '.avi', '.mov', '.wmv', '.flv')):
                         videos.append(file_name)
                     elif file_name.lower().endswith(('.jpg', '.jpeg', '.png', '.gif', '.bmp')):
                         images.append(file_name)
 
-                if videos or images:
+                if videos and images is not None:
                     try:
-                        print("Triggering next DAG")
+                        print(f"va a seguir el ciclo de detección de elementos")
                         trigger = TriggerDagRunOperator(
                             task_id='trigger_email_handler_inner',
                             trigger_dag_id='save_documents_to_minio',
-                            conf={'message': value_pulled},
+                            conf={'message': value_pulled}, 
                             execution_date=datetime.now().replace(tzinfo=timezone.utc),
                             dag=dag,
                         )
                         trigger.execute(context=kwargs)
                         Variable.delete("value")
                     except zipfile.BadZipFile:
-                        print("Error decoding Zip")
-                        raise AirflowSkipException("Error decoding Zip")
-                else:
-                    print("No relevant files found in ZIP")
+                        print(f"Error decoding Zip")
+                if videos and images is None:
+                    print(f"No va a seguir ningun ciclo")
                        
     except KeyError:
         print("Variable value does not exist")
@@ -109,85 +112,58 @@ def process_zip_file(**kwargs):
     except zipfile.BadZipFile:
         print("El archivo no es un ZIP válido")
         raise AirflowSkipException("El archivo no es un ZIP válido")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        raise AirflowSkipException("An unexpected error occurred")
+    Variable.set("key", None)   
 
-    finally:
-        Variable.set("key", None)   
 
 def process_tiff_file(**kwargs):
     try:
         value_pulled = Variable.get("value")
         print("Processing TIFF file")
-        # Aquí deberías implementar el procesamiento específico para archivos TIFF
     except KeyError:
         print("Variable value does not exist")
-        raise AirflowSkipException("Variable value does not exist")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        raise AirflowSkipException("An unexpected error occurred")
-    finally:
-        Variable.set("key", None)   
+        raise AirflowSkipException("Variable value does not exist")    
+    Variable.set("key", None)   
 
 def process_jpg_file(**kwargs):
     try:
         value_pulled = Variable.get("value")
         print("Processing JPG file")
-        # Aquí deberías implementar el procesamiento específico para archivos JPG
     except KeyError:
         print("Variable value does not exist")
         raise AirflowSkipException("Variable value does not exist")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        raise AirflowSkipException("An unexpected error occurred")
-    finally:
-        Variable.set("key", None)   
+    Variable.set("key", None)   
 
 def process_png_file(**kwargs):
     try:
         value_pulled = Variable.get("value")
         print("Processing PNG file")
-        # Aquí deberías implementar el procesamiento específico para archivos PNG
     except KeyError:
         print("Variable value does not exist")
         raise AirflowSkipException("Variable value does not exist")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        raise AirflowSkipException("An unexpected error occurred")
-    finally:
-        Variable.set("key", None)   
+    Variable.set("key", None)   
 
 def process_video_file(**kwargs):
     try:
         value_pulled = Variable.get("value")
         print("Processing Video file")
-        # Aquí deberías implementar el procesamiento específico para archivos de video
     except KeyError:
         print("Variable value does not exist")
         raise AirflowSkipException("Variable value does not exist")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
-        raise AirflowSkipException("An unexpected error occurred")
-    finally:
-        Variable.set("key", None)   
+    Variable.set("key", None)   
 
 def handle_unknown_file(**kwargs):
     print("Unknown file type")
     Variable.set("key", None)   
+
 
 def process_json_file(**kwargs):
     try:
         value_pulled = Variable.get("value")
         print("Processing JSON file")
         
-        if not value_pulled:
-            print("No data to process")
-            raise AirflowSkipException("No data to process")
-        
-        # Decodificar bytes si es necesario
+        # Verificar el tipo de dato
         if isinstance(value_pulled, bytes):
-            value_pulled = value_pulled.decode('utf-8').strip()
+            value_pulled = value_pulled.decode('utf-8').strip()  # Decodificar y eliminar espacios adicionales
 
         print(f"Content of JSON file: {value_pulled}")
 
@@ -208,9 +184,11 @@ def process_json_file(**kwargs):
     finally:
         Variable.set("key", None)
 
+
+
 default_args = {
     'owner': 'airflow',
-    'depends_on_past': False,
+    'depends_onpast': False,
     'start_date': datetime(2024, 7, 7),
     'email_on_failure': False,
     'email_on_retry': False,
