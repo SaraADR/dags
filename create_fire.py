cf46b6d9572cbf597f79eabfc7eb58587819b36d
SaraArribas <sara.arribas@cuatrodigital.com>
2024-07-24 10:53:00 +0200

    create fire

--- create_fire.py
+++ create_fire.py
@@ -3,6 +3,9 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 import ast
 import json
+import requests
+from requests.auth import HTTPBasicAuth
+from airflow.hooks.base import BaseHook
 
 
 def print_message(**context):
@@ -16,6 +19,31 @@ def create_fire(**context):
     input_data = json.loads(input_data_str)
     print(input_data)
     print(input_data['fire'])
+    print(input_data['type_id'])
+
+
+    if(input_data['type_id'] == 3):
+        #LLAMAR AL ATC
+        print("Es de tipo incendios") 
+        conn = BaseHook.get_connection('atc_services_connection') 
+        auth = (conn.login, conn.password)
+        # URL del servicio
+        url = f"{conn.host}/rest/FireService/save"
+        response = requests.post(url, json=input_data['fire'], auth=auth)
+        if response.status_code == 200:
+            print("Solicitud exitosa:")
+            print(response.json())
+        else:
+            print(f"Error en la solicitud: {response.status_code}")
+            print(response.text)
+    else:
+        #POR AHORA NADA
+        print("No es de tipo incendios")    
+
+
+
+
+
 
 
 
