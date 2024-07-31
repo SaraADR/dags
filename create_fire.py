d27cc3904fd941bd7aa15d29ff05e6722d1b3b5c
Óscar <oscar.lopez@cuatrodigital.com>
2024-07-31 08:48:00 +0200

    Actualización para que se inserten registros de incendio y misión con identificadores diferentes

--- create_fire.py
+++ create_fire.py
@@ -9,8 +9,6 @@ from airflow.hooks.base import BaseHook
 from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
 from sqlalchemy.orm import sessionmaker
 
-
-
 def print_message(**context):
     message = context['dag_run'].conf
     print(f"Received message: {message}")
@@ -24,11 +22,10 @@ def create_fire(**context):
     print(input_data['fire'])
     print(input_data['type_id'])
 
-
-    if(input_data['type_id'] == 3):
-        #LLAMAR AL ATC
-        print("Es de tipo incendios") 
-        conn = BaseHook.get_connection('atc_services_connection') 
+    if input_data['type_id'] == 3:
+        # LLAMAR AL ATC
+        print("Es de tipo incendios")
+        conn = BaseHook.get_connection('atc_services_connection')
         auth = (conn.login, conn.password)
         # URL del servicio
         url = f"{conn.host}/rest/FireService/save"
@@ -37,14 +34,14 @@ def create_fire(**context):
             print("Solicitud exitosa:")
             print(response.json())
 
+            # Llamar a create_mission con la respuesta del incendio y el job original
             create_mission(response.json(), input_data_str)
         else:
             print(f"Error en la solicitud: {response.status_code}")
             print(response.text)
     else:
-        #POR AHORA NADA
-        print("No es de tipo incendios")    
-
+        # POR AHORA NADA
+        print("No es de tipo incendios")
 
 def create_mission(fire, job):
     print(fire)
@@ -54,19 +51,19 @@ def create_mission(fire, job):
     engine = create_engine(connection_string)
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     try:
-        # Convert GeoJSON to WKT
+        # Convertir GeoJSON a WKT
         geojson_data = fire['position']
         geometry = geojson_to_wkt(geojson_data)
-
 
         values_to_insert = {
             'name': fire['name'],
             'start_date': fire['start'],
-            'geometry': geometry,  
+            'geometry': geometry,
             'type_id': 3, 
             'status_id': 1,
+            # Usar el ID del incendio como ID de la misión no es una práctica común pero se usa aquí para simplicidad del ejemplo
             'id': fire['id']
         }
 
@@ -76,14 +73,16 @@ def create_mission(fire, job):
         # Insertar los datos
         insert_stmt = missions.insert().values(values_to_insert)
         result = session.execute(insert_stmt)
-        print(result.inserted_primary_key)
+        mission_id = result.inserted_primary_key[0]  # Obtener el ID de la misión insertada
         session.commit()
-        session.close()
+        session.close()    
 
-        insert_relation_mission_fire(fire['id'], fire['id'])
+        # Insertar relación entre misión e incendio
+        insert_relation_mission_fire(mission_id, fire['id'])
+
     except Exception as e:
         session.rollback()
-        print("error durante el guardado de la misión")       
+        print(f"Error durante el guardado de la misión: {str(e)}")
 
 def insert_relation_mission_fire(id_mission, id_fire):
     db_conn = BaseHook.get_connection('biobd')
@@ -91,41 +90,37 @@ def insert_relation_mission_fire(id_mission, id_fire):
     engine = create_engine(connection_string)
     Session = sessionmaker(bind=engine)
     session = Session()
- 
+
     try:
-      
         values_to_insert = {
             'mission_id': id_mission,
             'fire_id': id_fire
         }
 
         metadata = MetaData(bind=engine)
-        missions = Table('mss_mission_fire', metadata, schema='missions', autoload_with=engine)
+        missions_fire = Table('mss_mission_fire', metadata, schema='missions', autoload_with=engine)
 
         # Insertar los datos
-        insert_stmt = missions.insert().values(values_to_insert)
+        insert_stmt = missions_fire.insert().values(values_to_insert)
         session.execute(insert_stmt)
         session.commit()
         session.close()
 
     except Exception as e:
         session.rollback()
-        print("error durante el guardado de la relacion mision-incendio") 
+        print(f"Error durante el guardado de la relación misión-incendio: {str(e)}")
 
 def geojson_to_wkt(geojson):
-        x = geojson['x']
-        y = geojson['y']
-        z = geojson['z']
+    x = geojson['x']
+    y = geojson['y']
+    z = geojson['z']
 
-        # Convert x and y to float if they are strings
-        x = float(x) if x is not None else None
-        y = float(y) if y is not None else None
-        if z is not None:
-            z = float(z) if z is not None else None
-            return f"POINT ({x} {y} {z})"
-        return f"POINT ({x} {y} {0})"
-
-
+    x = float(x) if x is not None else None
+    y = float(y) if y is not None else None
+    if z is not None:
+        z = float(z) if z is not None else None
+        return f"POINT ({x} {y} {z})"
+    return f"POINT ({x} {y} 0)"
 
 default_args = {
     'owner': 'airflow',
@@ -140,7 +135,7 @@ default_args = {
 dag = DAG(
     'create_fire',
     default_args=default_args,
-    description='DAG que envia emails',
+    description='DAG que maneja la creación de incendios y misiones',
     schedule_interval=None,
     catchup=False
 )
@@ -159,6 +154,5 @@ atc_create_fire_task = PythonOperator(
     provide_context=True,
     dag=dag,
 )
-
 
 print_message_task >> atc_create_fire_task
