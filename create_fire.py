731a0ba11a3563e24b6a54cca6e7061ced855b1b
Óscar <oscar.lopez@cuatrodigital.com>
2024-08-08 09:18:22 +0200

    Volver atras

--- create_fire.py
+++ create_fire.py
@@ -4,7 +4,7 @@ from airflow.operators.python import PythonOperator
 import json
 import requests
 from airflow.hooks.base import BaseHook
-from sqlalchemy import create_engine, Table, MetaData, Column, Integer, String, DateTime
+from sqlalchemy import create_engine, Table, MetaData
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from sqlalchemy.orm import sessionmaker
 
@@ -21,6 +21,7 @@ def create_mission(**context):
     input_data = json.loads(input_data_str)
     print(input_data)
     job_id = message['message']['id']  # Extracting job_id from the message
+
 
     try:
         # Conexión a la base de datos usando las credenciales almacenadas en Airflow
@@ -40,32 +41,148 @@ def create_mission(**context):
             'type_id': input_data['type_id'],
             'status_id': 1, #TODO REVISIÓN DE STATUS
             'customer_id': input_data ['customer_id'],
-            'execution_date': datetime.now() 
+            'execution_date': datetime.now()  # Adding current timestamp for execution_date
 
         }
 
-        # Inserción de la misión
-        metadata = MetaData()
-        missions = Table('missions', metadata, autoload_with=engine)
+        # Metadatos y tabla de misión en la base de datos
+        metadata = MetaData(bind=engine)
+        missions = Table('mss_mission', metadata, schema='missions', autoload_with=engine)
+
+        # Inserción de la nueva misión
         insert_stmt = missions.insert().values(values_to_insert)
         result = session.execute(insert_stmt)
         mission_id = result.inserted_primary_key[0]
-
-        # Inserción en la tabla mss_mission_status_history
-        mission_status_history = Table('mss_mission_status_history', metadata, autoload_with=engine)
-        status_history_insert = mission_status_history.insert().values({
-            'mission_id': mission_id,
-            'status': values_to_insert['status_id'],
-            'timestamp': datetime.now()
-        })
-        session.execute(status_history_insert)
         session.commit()
-
         session.close()
-        print(f"Misión creada con ID {mission_id} y guardada en la base de datos.")
+
+        print(f"Misión creada con ID: {mission_id}")
+
+        #  Almacenar mission_id en XCom para ser utilizado por otras tareas
+        input_data['mission_id'] = mission_id
+        context['task_instance'].xcom_push(key='mission_id', value=mission_id)
+
+        #TODO insertar status,mission_id en mission_status_history
+        #Igual hay que insertar el usuario
+        # Inserción en la tabla mission_status_history
+
+
+        # Crear el incendio relacionado
+        create_fire(input_data)
+
+        # Update job status to 'FINISHED'
+        jobs = Table('jobs', metadata, schema='public', autoload_with=engine)
+        update_stmt = jobs.update().where(jobs.c.id == job_id).values(status='FINISHED')
+        session.execute(update_stmt)
+        session.commit()
+        print(f"Job ID {job_id} status updated to FINISHED")
+        
+       
     except Exception as e:
         session.rollback()
-        print(f"Error durante la creación de la misión: {e}")
+        print(f"Error durante el guardado de la misión: {str(e)}")
+        jobs = Table('jobs', metadata, schema='public', autoload_with=engine)
+        update_stmt = jobs.update().where(jobs.c.id == job_id).values(status='RETRY')
+        session.execute(update_stmt)
+        session.commit()
+        print(f"Job ID {job_id} status updated to RETRY")
+
+# Función para crear un incendio a través del servicio ATC
+def create_fire(input_data):
+    try:
+        print("Creando incendio vía servicio ATC...")
+        # Conexión al servicio ATC usando las credenciales almacenadas en Airflow
+        conn = BaseHook.get_connection('atc_services_connection')
+        auth = (conn.login, conn.password)
+        url = f"{conn.host}/rest/FireService/save"
+        response = requests.post(url, json=input_data['fire'], auth=auth)
+
+        if response.status_code == 200:
+            print("Incendio creado con éxito.")
+            fire_data = response.json()
+            print(fire_data)
+
+            # Relacionar misión con incendio
+            insert_relation_mission_fire(input_data['mission_id'], fire_data['id'])
+        else:
+            print(f"Error en la creación del incendio: {response.status_code}")
+            print(response.text)
+
+    except Exception as e:
+        print(f"Error al crear el incendio: {str(e)}")
+
+# Función para insertar una relación entre misión e incendio en la base de datos
+def insert_relation_mission_fire(id_mission, id_fire):
+    try:
+        # Conexión a la base de datos usando las credenciales almacenadas en Airflow
+        db_conn = BaseHook.get_connection('biobd')
+        connection_string = f"postgresql://{db_conn.login}:{db_conn.password}@{db_conn.host}:{db_conn.port}/postgres"
+        engine = create_engine(connection_string)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+
+        values_to_insert = {
+            'mission_id': id_mission,
+            'fire_id': id_fire
+        }
+
+        # Metadatos y tabla de relación misión-incendio en la base de datos
+        metadata = MetaData(bind=engine)
+        missions_fire = Table('mss_mission_fire', metadata, schema='missions', autoload_with=engine)
+
+        # Inserción de la relación
+        insert_stmt = missions_fire.insert().values(values_to_insert)
+        session.execute(insert_stmt)
+        session.commit()
+        session.close()
+
+        print(f"Relación misión-incendio creada: misión {id_mission}, incendio {id_fire}")
+
+    except Exception as e:
+        session.rollback()
+        print(f"Error durante la relación misión-incendio: {str(e)}")
+
+# Función para convertir coordenadas GeoJSON a WKT
+def geojson_to_wkt(geojson):
+    x = geojson['x']
+    y = geojson['y']
+    z = geojson.get('z', 0)  # Proporcionar un valor predeterminado de 0 para z
+
+    x = float(x) if x is not None else None
+    y = float(y) if y is not None else None
+    z = float(z) if z is not None else 0
+
+    return f"POINT ({x} {y} {z})"
+
+# Función para enviar una notificación y almacenarla en la base de datos
+def send_notification(mission_id, message, status="enviada"):
+    db_conn = BaseHook.get_connection('biobd')
+    connection_string = f"postgresql://{db_conn.login}:{db_conn.password}@{db_conn.host}:{db_conn.port}/postgres"
+    engine = create_engine(connection_string)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        values_to_insert = {
+            'mission_id': mission_id,
+            'message': json.dumps(message),
+            'status': status,
+            'created_at': datetime.now()
+        }
+
+        # Metadatos y tabla de notificaciones en la base de datos
+        metadata = MetaData(bind=engine)
+        notifications = Table('notifications', metadata, schema='missions', autoload_with=engine)
+
+        # Inserción de la notificación
+        insert_stmt = notifications.insert().values(values_to_insert)
+        session.execute(insert_stmt)
+        session.commit()
+        session.close()
+        print("Notificación enviada y guardada en la base de datos.")
+    except Exception as e:
+        session.rollback()
+        print(f"Error durante el envío de la notificación: {e}")
 
 # Función para procesar una notificación después de la creación de una misión
 def process_notification(**context):
@@ -118,4 +235,4 @@ process_notification_task = PythonOperator(
 )
 
 # Definición de la secuencia de tareas en el DAG
-print_message_task >> create_mission_task >> process_notification_task
+print_message_task >> create_mission_task  >> process_notification_task
