29680ca0250929c4f751aa58c98eebe8a6c34f48
Óscar <oscar.lopez@cuatrodigital.com>
2024-08-20 12:15:08 +0200

    Test

--- create_fire.py
+++ create_fire.py
@@ -39,7 +39,6 @@ def create_mission(**context):
         values_to_insert = {
             'name': input_data['fire']['name'],
             'start_date': input_data['fire']['start'],
-            'ignition' : input_data['fire']['start'],
             'geometry': '{ "type": "Point", "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::4326" } }, "coordinates": [ '+input_data['fire']['position']['x']+', '+input_data['fire']['position']['y']+' ] }',
             'type_id': input_data['type_id'],
             'status_id': 1, #TODO REVISIÓN DE STATUS
