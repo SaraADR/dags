f990d73d26048c66ec6a93bb84bc7e54e31370c9
Óscar <oscar.lopez@cuatrodigital.com>
2024-08-23 11:59:00 +0200

    Simplificacion de procesos

--- create_bucket_minio.py
+++ create_bucket_minio.py
@@ -15,33 +15,30 @@ def create_bucket_in_minio(bucket_name):
     logging.info("Conexión a MinIO obtenida.")
 
     # Crear el cliente de MinIO/S3 con las credenciales y configuración necesarias
-    s3_client = boto3.client(
-        's3',
-        endpoint_url=extra['endpoint_url'],
-        aws_access_key_id=extra['aws_access_key_id'],
-        aws_secret_access_key=extra['aws_secret_access_key'],
-        config=Config(signature_version='s3v4')
-    )
+    try:
+        s3_client = boto3.client(
+            's3',
+            endpoint_url=extra['endpoint_url'],
+            aws_access_key_id=extra['aws_access_key_id'],
+            aws_secret_access_key=extra['aws_secret_access_key'],
+            config=Config(signature_version='s3v4')
+        )
+    except Exception as e:
+        logging.error(f"Error al crear el cliente de MinIO: {str(e)}")
+        raise
 
     logging.info(f"Intentando crear el bucket '{bucket_name}' en MinIO.")
 
-    # Crear el bucket si no existe
+    # Intentar crear el bucket directamente
     try:
-        s3_client.head_bucket(Bucket=bucket_name)
-        logging.info(f"El bucket '{bucket_name}' ya existe.")
+        s3_client.create_bucket(Bucket=bucket_name)
+        logging.info(f"Bucket '{bucket_name}' creado exitosamente en MinIO.")
     except ClientError as e:
-        error_code = int(e.response['Error']['Code'])
-        if error_code == 404:
-            # El bucket no existe, así que procedemos a crearlo
-            try:
-                s3_client.create_bucket(Bucket=bucket_name)
-                logging.info(f"Bucket '{bucket_name}' creado exitosamente en MinIO.")
-            except Exception as create_err:
-                logging.error(f"Error al crear el bucket: {str(create_err)}")
-                raise
-        else:
-            logging.error(f"Error al verificar el bucket: {str(e)}")
-            raise
+        logging.error(f"Error al crear el bucket: {e.response['Error']['Message']}")
+        raise
+    except Exception as e:
+        logging.error(f"Error inesperado al crear el bucket: {str(e)}")
+        raise
 
 default_args = {
     'owner': 'airflow',
@@ -56,8 +53,8 @@ dag = DAG(
     'create_bucket_in_minio',
     default_args=default_args,
     description='Un DAG para crear un bucket en Minio',
-    schedule_interval=None,  # No se repite automáticamente
-    catchup=False,  # Evita la ejecución retroactiva de períodos anteriores
+    schedule_interval=None,
+    catchup=False,
 )
 
 create_bucket_task = PythonOperator(
