ac93c2b2d46eb9332679182810453b9e35f45c10
SaraArribas <sara.arribas@cuatrodigital.com>
2024-09-03 19:56:09 +0200

    metadata 2.0

--- kafka_consumer_archivos_max.py
+++ kafka_consumer_archivos_max.py
@@ -101,6 +101,7 @@ def process_zip_file(value, **kwargs):
                         json_content = json.loads(content)
                         print(f"{content}")
                         for metadata in json_content.get('metadata', []):
+                            print(f"{metadata.get('name')}")
                             if metadata.get('name') == 'AlgorithmID':
                                 algorithm_id = metadata.get('value')
                             break
