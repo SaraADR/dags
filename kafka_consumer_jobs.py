560ab6484a1e1629fb8144fae8cd76f6c1e0fe3e
Óscar <oscar.lopez@cuatrodigital.com>
2024-09-04 13:43:34 +0200

    Cambiar los nombres de los trigger

--- kafka_consumer_jobs.py
+++ kafka_consumer_jobs.py
@@ -55,7 +55,7 @@ def trigger_email_handler(**kwargs):
             
             if msg_json.get('job') == 'heatmap-incendios':
                 trigger = TriggerDagRunOperator(
-                task_id='process_heatmap_data',
+                task_id='trigger_heatmap_handler_inner',
                 trigger_dag_id='heatmap_incendio_process',
                 conf={'message': msg_json}, 
                 execution_date=datetime.now().replace(tzinfo=timezone.utc),
