2bbbe07cb8fd158bd5021ab771198ceed5be5824
SaraArribas <sara.arribas@cuatrodigital.com>
2024-09-06 14:35:40 +0200

    bytes

--- vegetacion.py
+++ vegetacion.py
@@ -104,14 +104,14 @@ def process_extracted_files(**kwargs):
                     config=Config(signature_version='s3v4')
                 )
                 bucket_name = 'avincis-test'  
-
+                content_bytes = base64.b64decode(file['content'])
                 for file in files:
                     actual_child_key = str(unique_id_child) +'/' + file['file_name']
                     # Subir el archivo a MinIO
                     s3_client.put_object(
                         Bucket=bucket_name,
                         Key=actual_child_key,
-                        Body=io.BytesIO(file['content']),
+                        Body=io.BytesIO(content_bytes),
                     )
                     print(f'{file_name} subido correctamente a MinIO.')
 
