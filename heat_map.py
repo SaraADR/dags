4e19d6628edc981781364e83a81129d7cade2cd6
Óscar <oscar.lopez@cuatrodigital.com>
2024-09-20 08:58:09 +0200

    Test

--- heat_map.py
+++ heat_map.py
@@ -8,9 +8,10 @@ import boto3
 from botocore.client import Config
 from airflow.hooks.base_hook import BaseHook
 import os
-from sqlalchemy import Table, MetaData
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+import codecs
+import re
+import os
 
 from scriptConvertTIff import reproject_tiff
 
@@ -24,28 +25,51 @@ def process_heatmap_data(**context):
 
     # Realizar una operación condicional basada en el valor de 'type'
     if task_type == 'incendios':
+        # Lógica específica para el heatmap de incendios
         print("Procesando datos para el heatmap de incendios.")
+        # Modificaciones o lógica específica para incendios
+        # input_data["ar_incendios"] = "historical_fires.csv"
+        # input_data["url_search_fire"] = "https://pre.atcservices.cirpas.gal/rest/FireService/searchByIntersection"
+        # input_data["url_fireperimeter_service"] = "https://pre.atcservices.cirpas.gal/rest/FireAlgorithm_FirePerimeterService/getByFire?id="
+
     elif task_type == 'aeronaves':
+        # Lógica específica para el heatmap de aeronaves
         print("Procesando datos para el heatmap de aeronaves.")
+        # Modificaciones o lógica específica para aeronaves
+        # input_data["ar_aeronaves"] = "historical_aircraft.csv"
+        # input_data["url_search_aircraft"] = "https://pre.atcservices.cirpas.gal/rest/AircraftService/searchByIntersection"
+        # input_data["url_aircraftperimeter_service"] = "https://pre.atcservices.cirpas.gal/rest/AircraftAlgorithm_AircraftPerimeterService/getByAircraft?id="
+    
+    # El resto de tu código continúa aquí...
 
     message = context['dag_run'].conf
     input_data_str = message['message']['input_data']
     from_user = str(message['message']['from_user'])
-    job_id = message['message'].get('job_id')  # Asegúrate de obtener el ID del trabajo
-
     input_data = json.loads(input_data_str)
+
+   
+    # input_data["temp_tiff_path"] = TIFF2
     input_data["dir_output"] = "/home/airflow/workspace/output"
     input_data["user"] = "usuario"
     input_data["password"] = "contraseña"
 
+    # Aquí se ejecuta el algoritmo y deja de salida en el directorio 
+
+    # Log para verificar que los datos de entrada son correctos
     print("Datos completos de entrada:")
     print(json.dumps(input_data, indent=4))
 
     # Subir el archivo TIFF a MinIO
+
+    # cambiar_proyeccion_tiff(input_tiff=TIFF,output_tiff=TIFF2)
+    # output_tiff = crear el directorio del output tiff con uuid 
+
     tiff_key = f"{uuid.uuid4()}.tiff"
     with tempfile.TemporaryDirectory() as temp_dir:
         temp_dir_file = os.path.join(temp_dir, tiff_key)
+
         reproject_tiff(algorithm_output_tiff, temp_dir_file)
+        # input_data["temp_tiff_path"] = output_tiff
     
         try:
             connection = BaseHook.get_connection('minio_conn')
@@ -59,6 +83,7 @@ def process_heatmap_data(**context):
             )
 
             bucket_name = 'temp'
+            
             s3_client.upload_file(temp_dir_file, bucket_name, tiff_key)
             tiff_url = f"https://minioapi.avincis.cuatrodigital.com/{bucket_name}/{tiff_key}"
             print(f"Archivo TIFF subido correctamente a MinIO. URL: {tiff_url}")
@@ -72,48 +97,78 @@ def process_heatmap_data(**context):
             "to": from_user,
             "actions": [
                 {
-                    "type": "notify",
-                    "data": {
-                        "message": "Datos del heatmap procesados correctamente"
-                    }
+                "type": "notify",
+                "data": {
+                    "message": "Datos del heatmap procesados correctamente"
+                }
                 },
                 {
-                    "type": "paintTiff",
-                    "data": {
-                        "url": tiff_url
-                    }
+                "type": "paintTiff",
+                "data": {
+                    "url": tiff_url
+                }
                 }
             ]
         }
         notification_json = json.dumps(notification_db, ensure_ascii=False)
 
+        # Insertar la notificación en la base de datos PostgreSQL
         try:
             connection = BaseHook.get_connection('biobd')
-            engine = create_engine(connection.get_uri())  # Crear el engine usando SQLAlchemy
-            Session = sessionmaker(bind=engine)
-            session = Session()
-
-            # Insertar la notificación en la base de datos
-            session.execute(
-                f"INSERT INTO public.notifications (destination, data) VALUES ('ignis', '{notification_json}');"
+            pg_hook = PostgresOperator(
+                task_id='send_notification',
+                postgres_conn_id='biobd',
+                sql=f"""
+                INSERT INTO public.notifications (destination, data)
+                VALUES ('ignis', '{notification_json}');
+                """
             )
-            session.commit()
+            pg_hook.execute(context)
             print("Notificación almacenada correctamente en la base de datos.")
 
-            # **Actualización del estado del job a 'FINISHED'**
-            metadata = MetaData(bind=engine)
-            jobs = Table('jobs', metadata, schema='public', autoload_with=engine)
-            update_stmt = jobs.update().where(jobs.c.id == job_id).values(status='FINISHED')
-            session.execute(update_stmt)
-            session.commit()
-            print(f"Job ID {job_id} status updated to FINISHED")
-
         except Exception as e:
-            session.rollback()
-            print(f"Error al almacenar la notificación o actualizar el estado del job en la base de datos: {str(e)}")
+            print(f"Error al almacenar la notificación en la base de datos: {str(e)}")
 
-        finally:
-            session.close()
+
+# def cambiar_proyeccion_tiff(input_tiff, output_tiff):
+#     # Abrir el archivo TIFF
+#     dataset = gdal.Open(input_tiff, gdal.GA_Update)
+
+#     if dataset is None:
+#         raise FileNotFoundError(f"No se pudo abrir el archivo TIFF: {input_tiff}")
+
+#     # Obtener la proyección actual
+#     proyeccion = dataset.GetProjection()
+
+#     # Crear un objeto SpatialReference
+#     srs = osr.SpatialReference()
+
+#     # Revisar si ya tiene proyección
+#     if proyeccion:
+#         print(f"Proyección actual: {proyeccion}")
+
+#         # Si ya es EPSG:3857, no se cambia
+#         srs.ImportFromWkt(proyeccion)
+#         if srs.IsProjected() and srs.GetAttrValue("AUTHORITY", 1) == '3857':
+#             print("El archivo ya tiene la proyección EPSG:3857.")
+#         else:
+#             # Cambiar proyección a EPSG:3857
+#             print("Cambiando la proyección a EPSG:3857.")
+#             srs.ImportFromEPSG(3857)
+#             dataset.SetProjection(srs.ExportToWkt())
+#     else:
+#         # Si no tiene proyección, aplicar EPSG:3857
+#         print("No tiene proyección, aplicando EPSG:3857.")
+#         srs.ImportFromEPSG(3857)
+#         dataset.SetProjection(srs.ExportToWkt())
+
+#     # Guardar el archivo TIFF con la nueva proyección
+#     gdal.Warp(output_tiff, dataset, dstSRS='EPSG:3857')
+
+#     # Cerrar el dataset
+#     dataset = None
+
+#     print(f"Se ha guardado el archivo con la proyección EPSG:3857 en: {output_tiff}")
 
 
 # Configuración del DAG
