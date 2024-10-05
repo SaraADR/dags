ea38ae862ff16b90e84db36fa2c5d0e40e6e64b6
SaraArribas <sara.arribas@cuatrodigital.com>
2024-10-05 15:47:26 +0200

    sin gdal

--- heat_map.py
+++ heat_map.py
@@ -20,7 +20,7 @@ from airflow.operators.dagrun_operator import TriggerDagRunOperator
 from datetime import datetime, timedelta, timezone
 from airflow.providers.ssh.hooks.ssh import SSHHook
 from scriptConvertTIff import reproject_tiff
-import gdal, osr
+
 
 
 def process_heatmap_data(**context):
@@ -225,45 +225,45 @@ def change_state_job(**context):
         session.rollback()
         print(f"Error durante el guardado del estado del job: {str(e)}")
 
-def cambiar_proyeccion_tiff(input_tiff, output_tiff):
-    # Abrir el archivo TIFF
-    dataset = gdal.Open(input_tiff, gdal.GA_Update)
+# def cambiar_proyeccion_tiff(input_tiff, output_tiff):
+#     # Abrir el archivo TIFF
+#     dataset = gdal.Open(input_tiff, gdal.GA_Update)
 
-    if dataset is None:
-        raise FileNotFoundError(f"No se pudo abrir el archivo TIFF: {input_tiff}")
+#     if dataset is None:
+#         raise FileNotFoundError(f"No se pudo abrir el archivo TIFF: {input_tiff}")
 
-    # Obtener la proyección actual
-    proyeccion = dataset.GetProjection()
+#     # Obtener la proyección actual
+#     proyeccion = dataset.GetProjection()
 
-    # Crear un objeto SpatialReference
-    srs = osr.SpatialReference()
+#     # Crear un objeto SpatialReference
+#     srs = osr.SpatialReference()
 
-    # Revisar si ya tiene proyección
-    if proyeccion:
-        print(f"Proyección actual: {proyeccion}")
+#     # Revisar si ya tiene proyección
+#     if proyeccion:
+#         print(f"Proyección actual: {proyeccion}")
 
-        # Si ya es EPSG:3857, no se cambia
-        srs.ImportFromWkt(proyeccion)
-        if srs.IsProjected() and srs.GetAttrValue("AUTHORITY", 1) == '3857':
-            print("El archivo ya tiene la proyección EPSG:3857.")
-        else:
-            # Cambiar proyección a EPSG:3857
-            print("Cambiando la proyección a EPSG:3857.")
-            srs.ImportFromEPSG(3857)
-            dataset.SetProjection(srs.ExportToWkt())
-    else:
-        # Si no tiene proyección, aplicar EPSG:3857
-        print("No tiene proyección, aplicando EPSG:3857.")
-        srs.ImportFromEPSG(3857)
-        dataset.SetProjection(srs.ExportToWkt())
+#         # Si ya es EPSG:3857, no se cambia
+#         srs.ImportFromWkt(proyeccion)
+#         if srs.IsProjected() and srs.GetAttrValue("AUTHORITY", 1) == '3857':
+#             print("El archivo ya tiene la proyección EPSG:3857.")
+#         else:
+#             # Cambiar proyección a EPSG:3857
+#             print("Cambiando la proyección a EPSG:3857.")
+#             srs.ImportFromEPSG(3857)
+#             dataset.SetProjection(srs.ExportToWkt())
+#     else:
+#         # Si no tiene proyección, aplicar EPSG:3857
+#         print("No tiene proyección, aplicando EPSG:3857.")
+#         srs.ImportFromEPSG(3857)
+#         dataset.SetProjection(srs.ExportToWkt())
 
-    # Guardar el archivo TIFF con la nueva proyección
-    gdal.Warp(output_tiff, dataset, dstSRS='EPSG:3857')
+#     # Guardar el archivo TIFF con la nueva proyección
+#     gdal.Warp(output_tiff, dataset, dstSRS='EPSG:3857')
 
-    # Cerrar el dataset
-    dataset = None
+#     # Cerrar el dataset
+#     dataset = None
 
-    print(f"Se ha guardado el archivo con la proyección EPSG:3857 en: {output_tiff}")
+#     print(f"Se ha guardado el archivo con la proyección EPSG:3857 en: {output_tiff}")
 
 
 
