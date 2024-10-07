97dc3a277cc54d23e8ccca4ca7641861d154a321
Óscar <oscar.lopez@cuatrodigital.com>
2024-10-07 11:02:38 +0200

    Update files

--- metashape_rgb.py
+++ metashape_rgb.py
@@ -140,6 +140,8 @@ def upload_to_geonetwork(**context):
 
         # Obtener el XML base64 desde XCom
         xml_encoded = context['ti'].xcom_pull(task_ids='generate_xml')
+        xml_decoded = base64.b64decode(xml_encoded)
+
         
         files = {
             'metadataType': (None, 'METADATA'),
@@ -147,7 +149,8 @@ def upload_to_geonetwork(**context):
             'transformWith': (None, 'none'),
             'group': (None, '2'),  # Cambia el valor de 'group' si es necesario
             'category': (None, ''),  # Si no tienes categoría, puede ir vacío
-            'file': ('nombre_archivo.xml', open(xml_encoded), 'rb')
+            'file': ('nombre_archivo.xml', xml_decoded)
+
         }
 
         # URL de GeoNetwork para subir el archivo XML (Move this line up)
