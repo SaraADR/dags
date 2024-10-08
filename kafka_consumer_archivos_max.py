11db64c11747472061bdc203e81cad7eaa17856b
SaraArribas <sara.arribas@cuatrodigital.com>
2024-10-08 11:04:06 +0200

    end of batch

--- kafka_consumer_archivos_max.py
+++ kafka_consumer_archivos_max.py
@@ -222,7 +222,7 @@ consume_from_topic = ConsumeFromTopicOperator(
     topics=["sftp2"],
     apply_function=consumer_function,
     apply_function_kwargs={"prefix": "consumed:::"},
-    commit_cadence="message",
+    commit_cadence="end_of_batch",
     dag=dag,
 )
 
