500b68d84656a4aeb39b2bf162c7a4253d10153e
Oscarlbs <oscarlopezboadosanmartin@gmail.com>
2024-10-11 12:51:54 +0200

    Version de pruebas estable

--- metashape_rgb.py
+++ metashape_rgb.py
@@ -9,8 +9,6 @@ import requests
 import logging
 import io  # Para manejar el archivo XML en memoria
 import re
-from pyproj import CRS
-
 
 # Configurar la URL de GeoNetwork
 geonetwork_url = "https://eiiob.dev.cuatrodigital.com/geonetwork/srv/api"
@@ -43,7 +41,6 @@ def convertir_coords(epsg_input,south, west, north, east):
     
 
 def generate_xml(**kwargs):
-    
     logging.info("Iniciando la generación del XML.")
 
     xml_encoded = []
@@ -53,31 +50,19 @@ def generate_xml(**kwargs):
 
     logging.info(f"Contenido JSON cargado: {algoritm_result}")
 
-    # Obtener los metadatos estáticos
-    organization_name = 'Avincis'
-    email_address = 'avincis@organizacion.es'
-    protocol = 'OGC:WMS-1.3.0-http-get-map'
-    wms_base_link = 'https://geoserver.dev.cuatrodigital.com/geoserver/tests-geonetwork/wms'
-    file_identifier_base = "Ortomosaico_testeo"  # Un identificador base, que puede ajustarse
-
-    # Datos del algoritmo
     executionResources = algoritm_result['executionResources']
-    executionArguments = algoritm_result['executionArguments']
-    metadata = algoritm_result['metadata']
-
-    # Fechas y datos misceláneos
-    publication_date = metadata[0]['value']  # Suponiendo que 'dateStampDate' es el primer elemento
-    mission_id = next((m for m in metadata if m['name'] == 'MissionID'), None)['value']
-    sensor_sn = next((m for m in metadata if m['name'] == 'SensorSN'), None)['value']
-    aircraft_number = next((m for m in metadata if m['name'] == 'AircraftNumberPlate'), None)['value']
-    pilot_name = next((m for m in metadata if m['name'] == 'PilotName'), None)['value']
-    operator_name = next((m for m in metadata if m['name'] == 'OperatorName'), None)['value']
 
     # Se extrae la información del BBOX y el sistema de referencia
     outputFalse = next((obj for obj in executionResources if obj["output"] == False), None)["data"]
     bboxData = next((obj for obj in outputFalse if obj["type"] == "BBOX"), None)
     bbox = bboxData["value"]
     coordinate_system = bboxData["ReferenceSystem"]
+
+    # Datos que no varían
+    organization_name = 'Avincis'
+    email_address = 'avincis@organizacion.es'
+    protocol = 'OGC:WMS-1.3.0-http-get-map'
+    wms_link = 'https://geoserver.dev.cuatrodigital.com/geoserver/tests-geonetwork/wms'
 
     # Coords BBOX
     west_bound_pre = bbox['westBoundLongitude']
@@ -96,7 +81,6 @@ def generate_xml(**kwargs):
         if not re.search(r'\.tif$', resource['path'], re.IGNORECASE):
             continue
 
-        # Extraer la información relevante del recurso
         identifier = next((obj for obj in resource['data'] if obj['name'] == 'identifier'), None)["value"]
         spatial_resolution = next((obj for obj in resource['data'] if obj['name'] == 'pixelSize'), None)["value"]
         specificUsage = next((obj for obj in resource['data'] if obj['name'] == 'specificUsage'), None)["value"]
@@ -105,11 +89,12 @@ def generate_xml(**kwargs):
         layer_name = identifier
         title = identifier
 
-        # Asignar un WMS link para este recurso (puedes modificar esto según el contexto)
-        wms_link = wms_base_link + f"?layer={layer_name}"
-        layer_description = "Descripción de la capa generada"  # Descripción genérica
-        file_identifier = f"{file_identifier_base}_{identifier}"  # Un identificador único (se puede derivar)
+        # JSON dinámico con los valores correspondientes
+        wms_link = algoritm_result['executionResources'][0]['path']  # Link de WMS para este recurso específico
+        layer_description = "Descripción de la capa generada"  # Puedes extraer o generar esto según el contexto
+        file_identifier = "Ortomosaico_testeo"  # Un identificador único (se puede derivar)
         date_stamp = datetime.now().isoformat()
+        publication_date = "2024-07-29"  # Basado en la fecha proporcionada en el archivo
 
         logging.info("Llamando a la función creador_xml_metadata.")
         
