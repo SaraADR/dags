477d2fafb15dcde92f4b0afa185145bb424eeab4
SaraArribas <sara.arribas@cuatrodigital.com>
2024-11-04 10:32:19 +0100

    prints

--- zips_no_algoritmos.py
+++ zips_no_algoritmos.py
@@ -141,6 +141,9 @@ def process_zip_file(local_zip_path, nombre_fichero, message, **kwargs):
                                 print(docker_command)
 
                                 stdin, stdout, stderr = ssh_client.exec_command(docker_command)
+                                print(stdin)
+                                print(stdout)
+                                print(stderr)
 
                                 # Attempt decoding output with UTF-8, fallback to latin-1 if necessary
                                 try:
