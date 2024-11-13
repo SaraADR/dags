ba90b50877f3e3fc4c4de8e0efd1d1aa2c32754a
SaraArribas <sara.arribas@cuatrodigital.com>
2024-11-13 11:52:05 +0100

    timestamp bug

--- zips_no_algoritmos.py
+++ zips_no_algoritmos.py
@@ -258,10 +258,10 @@ def save_data(data_json):
 
 def convert_timestamp(timestamp_str):
     try:
-        return datetime.strptime(timestamp_str, '%Y:%m:%d %H:%M:%S%z')
+        return datetime.strptime(timestamp_str, '%Y:%m:%d %H:%M:%S%z') if timestamp_str else None
     except Exception as e:
         print(f"Error parsing timestamp: {e}")
-        return None  
+        return None
 
 default_args = {
     'owner': 'sadr',
