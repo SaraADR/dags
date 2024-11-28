0e270bed3fe2e6394dda2110a5805ef7b6f1f603
SaraAdr <sara.arrdr@gmail.com>
2024-11-28 15:53:01 +0100

    fechas

--- test_recogida_metadatos.py
+++ test_recogida_metadatos.py
@@ -203,13 +203,17 @@ def is_visible_or_ter(message, local_zip_path, output, output_json, type):
 
         
         try:
-            if(type != -1):
+            if(type == 2): #Multiespectral
+                date_time_str = output_json.get("DateTimeOriginal")
+                timestamp_naive = datetime.strptime(date_time_str, "%Y:%m:%d %H:%M:%S")
+            elif(type == -1):
+                date_time_str = output_json.get("xmp:dateTimeOriginal")
+                date_time_original = datetime.strptime(date_time_str, "%Y-%m-%dT%H:%M")
+            else:
                 date_time_str = output_json.get("DateTimeOriginal")
                 date_time_original = datetime.strptime(date_time_str, "%Y:%m:%d %H:%M:%S%z")
                 timestamp_naive = date_time_original.replace(tzinfo=None)
-            if(type == -1):
-                date_time_str = output_json.get("xmp:dateTimeOriginal")
-                date_time_original = datetime.strptime(date_time_str, "%Y-%m-%dT%H:%M")
+
         except ValueError as ve:
             print(f"Error al parsear la fecha '{date_time_str}': {ve}")
             raise
