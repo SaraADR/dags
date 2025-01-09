dae0f7e1825bad00473dd63ecd5cb656bc1b6eed
Óscar <oscar.lopez@cuatrodigital.com>
2025-01-09 11:41:10 +0100

    Pruebas con el manejo de errores

--- algorithm_heatmap_post_process.py
+++ algorithm_heatmap_post_process.py
@@ -21,31 +21,6 @@ from rasterio.warp import calculate_default_transform, reproject, Resampling
 import numpy as np
 
 
-# Función para registrar el estado de error en la tabla 'jobs'
-def update_job_status_to_error(job_id, error_message):
-    try:
-        db_conn = BaseHook.get_connection('biobd')
-        connection_string = f"postgresql://{db_conn.login}:{db_conn.password}@{db_conn.host}:{db_conn.port}/postgres"
-        engine = create_engine(connection_string)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
-        # Actualizar el estado a ERROR y guardar el mensaje de error
-        metadata = MetaData(bind=engine)
-        jobs = Table('jobs', metadata, schema='public', autoload_with=engine)
-        update_stmt = jobs.update().where(jobs.c.id == job_id).values(
-            status='ERROR',
-            output_data=json.dumps({"error": error_message})
-        )
-        session.execute(update_stmt)
-        session.commit()
-        print(f"Job ID {job_id} status updated to ERROR.")
-    except Exception as e:
-        print(f"Error al actualizar el estado del trabajo a ERROR: {str(e)}")
-    finally:
-        session.close()
-
-
 def process_heatmap_data(**context):
 
     # Obtener el valor de 'type' de default_args a través del contexto
@@ -58,10 +33,8 @@ def process_heatmap_data(**context):
     isIncendio = "FALSE"
     arincendios = ''
     if task_type == 'heatmap-incendios':
-        raise ValueError("Fallo forzado para probar la gestión de errores.")
-
         # Lógica específica para el heatmap de incendios
-        print("Procesando datos para el heatmap de incendios.")
+        raise RuntimeError("Error intencional para prueba.")
   
         # Modificaciones o lógica específica para incendios
         arincendios = "historical_fires.csv"
@@ -78,15 +51,7 @@ def process_heatmap_data(**context):
         isIncendio = "FALSE"
         # input_data["url_search_aircraft"] = "https://pre.atcservices.cirpas.gal/rest/AircraftService/searchByIntersection"
         # input_data["url_aircraftperimeter_service"] = "https://pre.atcservices.cirpas.gal/rest/AircraftAlgorithm_AircraftPerimeterService/getByAircraft?id="
-    
-    
-    # Convertir las fechas a datetime
-    # low_search_date = datetime.strptime(input_data['lowSearchDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
-    # high_search_date = datetime.strptime(input_data['highSearchDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
 
-    # # Formatear las fechas al nuevo formato con zona horaria '+0000'
-    # input_data['lowSearchDate'] = low_search_date.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + '+0000'
-    # input_data['highSearchDate'] = high_search_date.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + '+0000'
 
 
     if 'lonlat' in input_data and len(input_data['lonlat']) == 4:
@@ -201,7 +166,7 @@ def process_heatmap_data(**context):
                 # Lanzar la excepción para que la tarea falle
                 raise RuntimeError(f"Error durante el guardado de la misión")
 
-        
+            
             else:
                 output_directory = '/home/admin3//Algoritmo_mapas_calor/algoritmo-mapas-de-calor-objetivo-1-master/output/' + str(task_type) + '_' + str(message['message']['id'])
                 local_output_directory = '/tmp' + '/' + str(message['message']['id'])
@@ -258,12 +223,38 @@ def process_heatmap_data(**context):
             up_to_minio(local_output_directory, from_user, isIncendio, '/tmp')
 
     except Exception as e:
-        error_message = f"Error en process_heatmap_data: {str(e)}"
-        print(error_message)
-        # Actualizar el estado a ERROR en la tabla 'jobs'
-        update_job_status_to_error(job_id, error_message)
-        # Relanzar la excepción para que la tarea de Airflow falle también
+        print(f"Error durante el proceso: {e}")
+        # Actualizar el estado del trabajo a ERROR
+        update_job_status(job_id, "ERROR", {"error": str(e)})
+        print(f"Estado actualizado a ERROR para Job ID: {job_id}")
         raise
+
+
+def update_job_status(job_id, status, output_data=None):
+    """
+    Actualiza el estado del trabajo en la tabla `jobs`.
+    """
+    print(f"Iniciando actualización de estado para Job ID: {job_id}...")
+    db_conn = BaseHook.get_connection('biobd')
+    connection_string = f"postgresql://{db_conn.login}:{db_conn.password}@{db_conn.host}:{db_conn.port}/postgres"
+    print(f"Conexión a la base de datos: {connection_string}")
+
+    engine = create_engine(connection_string)
+    with engine.connect() as connection:
+        query = text("""
+            UPDATE public.jobs
+            SET status = :status, output_data = :output_data, execution_date = :execution_date
+            WHERE id = :job_id;
+        """)
+        connection.execute(query, {
+            'status': status,
+            'output_data': json.dumps(output_data) if output_data else None,
+            'execution_date': datetime.now(timezone.utc),
+            'job_id': job_id
+        })
+        print(f"Estado del trabajo {job_id} actualizado a {status}.")
+
+
 
 def up_to_minio(local_output_directory, from_user, isIncendio, temp_dir):
     key = f"{uuid.uuid4()}"
@@ -319,6 +310,8 @@ def up_to_minio(local_output_directory, from_user, isIncendio, temp_dir):
     except Exception as e:
         print(f"Error al subir archivos a MinIO: {str(e)}")
 
+
+
     try:
         db_conn = BaseHook.get_connection('biobd')
         connection_string = f"postgresql://{db_conn.login}:{db_conn.password}@{db_conn.host}:{db_conn.port}/postgres"
@@ -362,6 +355,26 @@ def up_to_minio(local_output_directory, from_user, isIncendio, temp_dir):
         print(f"Error durante la inserción de la notificación: {str(e)}")
     finally:
         session.close()
+
+
+
+
+    # Subir el archivo TIFF a MinIO
+
+    # cambiar_proyeccion_tiff(input_tiff=TIFF,output_tiff=TIFF2)
+    # output_tiff = crear el directorio del output tiff con uuid 
+
+
+
+    # tiff_key = f"{uuid.uuid4()}.tiff"
+    # with tempfile.TemporaryDirectory() as temp_dir:
+    #     temp_dir_file = os.path.join(temp_dir, tiff_key)
+
+    #     reproject_tiff(algorithm_output_tiff, temp_dir_file)
+    #     # input_data["temp_tiff_path"] = output_tiff
+    
+
+
 
 def change_state_job(**context):
     message = context['dag_run'].conf
@@ -504,8 +517,11 @@ default_args = {
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=1),
+    'type': 'incendios',
 }
 
+
+# Definición del DAG incendios
 dag = DAG(
     'algorithm_heatmap_post_process',
     default_args=default_args,
@@ -523,5 +539,13 @@ process_heatmap_task = PythonOperator(
     dag=dag,
 )
 
+#Cambia estado de job
+change_state_task = PythonOperator(
+    task_id='change_state_job',
+    python_callable=change_state_job,
+    provide_context=True,
+    dag=dag,
+)
+
 # Ejecución de la tarea en el DAG
-process_heatmap_task
+process_heatmap_task >> change_state_task
