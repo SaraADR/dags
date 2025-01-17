53f9e51fe2fbe90d0bb8240a2c31999588c5cbc3
SaraAdr <sara.arrdr@gmail.com>
2025-01-17 12:18:05 +0100

    rutas de escape

--- algorithm_escape_routes_post_process.py
+++ algorithm_escape_routes_post_process.py
@@ -167,7 +167,9 @@ def process_escape_routes_data(**context):
 
             ssh_client.exec_command(f"touch -p {json_file_path}")
             ssh_client.exec_command(f"chmod 644 {json_file_path}")
-            
+
+            print(json_file)
+
             with sftp.file(json_file_path, 'w') as json_file:
                 json.dumps(json_data, json_file, indent=4)
             print(f"Archivo JSON guardado en: {json_file_path}")
