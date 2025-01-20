1f0b251271f024d0ab20b08e5a7dbdd56426460f
SaraAdr <sara.arrdr@gmail.com>
2025-01-20 16:50:19 +0100

    test other log

--- algorithm_escape_routes_post_process.py
+++ algorithm_escape_routes_post_process.py
@@ -230,6 +230,10 @@ def process_escape_routes_data(**context):
             command = (
                 f' CONFIGURATION_PATH={json_file_path} docker-compose -f "./algoritmo_rutas_escape/launch/compose.yaml" up --build --abort-on-container-exit && docker-compose -f compose.yaml down --volumes'
             )
+            command = (
+                f'docker-compose -f compose.yaml up --build --abort-on-container-exit && docker-compose -f compose.yaml down --volumes'
+            )
+            
             stdin, stdout, stderr = ssh_client.exec_command(command)
             output = stdout.read().decode()
             error_output = stderr.read().decode()
