7bf5aba0a4512e02dd622919b36e3a72fd693f7a
SaraAdr <sara.arrdr@gmail.com>
2025-01-21 11:58:39 +0100

    .

--- algorithm_escape_routes_post_process.py
+++ algorithm_escape_routes_post_process.py
@@ -192,31 +192,6 @@ def process_escape_routes_data(**context):
         "dist_estudio": input_data.get('dist_estudio', 5000),
     }
 
-    # params = {
-    #     "directorio_alg" : ".",
-    #     "dir_output": f"/share_data/output/rutas_escape_{str(message['message']['id'])}",
-    #     "dir_incendio": f"{json_file_path}",
-    #     "dir_mdt": input_data.get('dir_mdt', None),
-    #     "dir_hojasmtn50": file_paths["dir_hojasmtn50"],
-    #     "dir_combustible": "/share_data/input/modelos_combustible_Galicia_2020.tif",
-    #     "api_idee": True,
-    #     "dir_vias": file_paths["dir_vias"],
-    #     "dir_cursos_agua": file_paths["dir_cursos_agua"],
-    #     "dir_aguas_estancadas": file_paths["dir_aguas_estancadas"],
-    #     "inicio": '{"latitude": 42.275683, "longitude": -8.082887}', 
-    #     "destino": '{"latitude": 42.27571, "longitude":-8.0885}',  
-    #     "direccion_avance": 90,
-    #     "dist_seguridad": input_data.get('dist_seguridad', None),
-    #     "dir_obstaculos": input_data.get('dir_obstaculos', None),
-    #     "dir_carr_csv": file_paths["dir_carr_csv"],
-    #     "sugerir": input_data.get('sugerir', False),
-    #     "zonas_abiertas": file_paths["zonas_abiertas"],
-    #     "v_viento": input_data.get('v_viento', None),
-    #     "f_buffer": 100,
-    #     "c_prop": input_data.get('c_prop', None),
-    #     "lim_pendiente": input_data.get('lim_pendiente', 90),
-    #     "dist_estudio": input_data.get('dist_estudio', 3000),
-    # }
 
     json_data = create_json(params)
 
@@ -245,7 +220,8 @@ def process_escape_routes_data(**context):
             print("Contenido del archivo JSON:")
             print(json_content)
 
-                    # Validar JSON
+
+            # Validar JSON
             with sftp.file(json_file_path, 'r') as json_file:
                 json_data = json.load(json_file)
                 print("Contenido del JSON válido:", json.dumps(json_data, indent=4))
@@ -253,7 +229,6 @@ def process_escape_routes_data(**context):
             command = (
                 f'cd /home/admin3/algoritmo_rutas_escape && docker-compose -f launch/compose.yaml up --build'
             )
-
 
             stdin, stdout, stderr = ssh_client.exec_command(command)
             output = stdout.read().decode()
@@ -268,7 +243,6 @@ def process_escape_routes_data(**context):
 
 
             output_directory = f'/home/admin3/algoritmo_rutas_escape/output/rutas_escape_{str(message['message']['id'])}' 
-            output_directory = f'/home/admin3/algoritmo_rutas_escape/output/Test_7' 
             local_output_directory = '/tmp'
 
             sftp.chdir(output_directory)
