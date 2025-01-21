fa4a4d0a5690cf67ffeb60c8f4df35a0a218f6d7
SaraAdr <sara.arrdr@gmail.com>
2025-01-21 20:56:48 +0100

    2.3version

--- test_recogida_metadatos.py
+++ test_recogida_metadatos.py
@@ -246,7 +246,7 @@ def is_visible_or_ter(message, local_zip_path, output, output_json, type):
     # Buscar los metadatos en captura
     try:
         db_conn = BaseHook.get_connection('biobd')
-        connection_string = f"postgresql://{db_conn.login}:{db_conn.password}@{db_conn.host}:{db_conn.port}/v2.3"
+        connection_string = f"postgresql://{db_conn.login}:{db_conn.password}@{db_conn.host}:{db_conn.port}/postgres"
         engine = create_engine(connection_string)
         Session = sessionmaker(bind=engine)
         session = Session()
