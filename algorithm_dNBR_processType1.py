b29aa89c35c3af3ea37e54688deb94e426834b47
SaraAdr <sara.arrdr@gmail.com>
2025-01-30 17:01:10 +0100

    limpieza de codigo

--- algorithm_dNBR_processType1.py
+++ algorithm_dNBR_processType1.py
@@ -31,35 +31,6 @@ def process_element(**context):
     print(result)
 
 
-    # ssh_hook = SSHHook(ssh_conn_id='my_ssh_conn')
-
-    # try:
-    #     # Conectarse al servidor SSH
-    #     with ssh_hook.get_conn() as ssh_client:
-    #         sftp = ssh_client.open_sftp()
-    #         print(f"Sftp abierto")
-
-
-    #         remote_directory = '/home/admin3/algoritmo-calculo-dNBR/input'
-    #         remote_file_name = 'Test_2_1.json'
-    #         remote_file_path = os.path.join(remote_directory, remote_file_name)
-    #         sftp.chdir(remote_directory)
-    #         print(f"Cambiando al directorio: {remote_directory}")
-
-    #         output_directory = '/home/admin3/algoritmo-calculo-dNBR/output/test_2_1'
-    #         local_output_directory = '/tmp'
-
-    #         # Crear el directorio local si no existe
-    #         os.makedirs(local_output_directory, exist_ok=True)
-    #         sftp.chdir(output_directory)
-    #         print(f"Cambiando al directorio de salida: {output_directory}")
-    #         sftp.close()
-    # except Exception as e:
-    #     print(f"Error: {str(e)}")    
-    #     sftp.close()
-    # return
-
-
 default_args = {
     'owner': 'sadr',
     'depends_on_past': False,
