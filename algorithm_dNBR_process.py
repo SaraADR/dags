428cd6f1528635687a6edc1eb7de49b8584da533
SaraAdr <sara.arrdr@gmail.com>
2025-01-30 10:57:08 +0100

    query dNBR

--- algorithm_dNBR_process.py
+++ algorithm_dNBR_process.py
@@ -7,6 +7,7 @@ from airflow.hooks.base_hook import BaseHook
 import json
 import pytz
 from airflow.models import Variable
+from dag_utils import execute_query
 
 def process_element(**context):
 
@@ -15,8 +16,25 @@ def process_element(**context):
 
     print(f"Este algoritmo se está ejecutando a las {fechaHoraActual.strftime('%Y-%m-%d %H:%M:%S')} en Madrid, España")
 
-    mi_variable = Variable.get("dNBR_diasFinIncendio", default_var="10")
-    print(f"Valor de la variable en Airflow: {mi_variable}")
+    tipo1diasincendio = Variable.get("dNBR_diasFinIncendio", default_var="10")
+    print(f"Valor de la variable tipo1diasincendio en Airflow: {tipo1diasincendio}")
+
+    tipo2mesesminimo = Variable.get("dNBR_mesesFinIncendioMinimo", default_var="3")
+    print(f"Valor de la variable tipo2mesesminimo en Airflow: {tipo2mesesminimo}")
+
+    tipo2mesesmaximo = Variable.get("dNBR_mesesFinIncendioMaximo", default_var="10000")
+    print(f"Valor de la variable tipo2mesesmaximo en Airflow: {tipo2mesesmaximo}")
+
+    query = """
+        SELECT m.id , mf.fire_id, m.start_date , m.end_date
+        FROM public.mss_mission m
+        JOIN public.mss_mission_fire mf ON m.id = mf.mission_id
+        WHERE m.end_date <= NOW() - INTERVAL ':days days'
+    """
+    params = {'days': tipo1diasincendio}
+    result = execute_query('biobd', query, params)
+    print(result)
+
 
     # ssh_hook = SSHHook(ssh_conn_id='my_ssh_conn')
 
