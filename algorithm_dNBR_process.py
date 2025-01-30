f4e0fa9136be900a1d445e81c9031a08ded6552e
SaraAdr <sara.arrdr@gmail.com>
2025-01-30 12:28:29 +0100

    query

--- algorithm_dNBR_process.py
+++ algorithm_dNBR_process.py
@@ -25,14 +25,15 @@ def process_element(**context):
     tipo2mesesmaximo = Variable.get("dNBR_mesesFinIncendioMaximo", default_var="10000")
     print(f"Valor de la variable tipo2mesesmaximo en Airflow: {tipo2mesesmaximo}")
 
-    query = """
-        SELECT m.id , mf.fire_id, m.start_date , m.end_date
+    interval_value = f'{tipo1diasincendio} days'
+    query = f"""
+        SELECT m.id, mf.fire_id, m.start_date, m.end_date
         FROM missions.mss_mission m
         JOIN missions.mss_mission_fire mf ON m.id = mf.mission_id
-        WHERE m.end_date <= NOW() - INTERVAL :days days'
+        WHERE m.end_date <= NOW() - INTERVAL '{interval_value}'
     """
-    params = {'days': int(tipo1diasincendio)}
-    result = execute_query('biobd', query, params)
+
+    result = execute_query('biobd', query)
     print(result)
 
 
