3b269b730ec283390736f8f075aeb054a3840023
SaraAdr <sara.arrdr@gmail.com>
2025-02-14 11:25:42 +0100

    test

--- algorithm_dNBR_processType1.py
+++ algorithm_dNBR_processType1.py
@@ -90,13 +90,13 @@ def ejecutar_algoritmo(datos, fechaHoraActual):
 
                 params = {          
                     "directorio_alg":".",
-                    "directorio_output":"/share_data/output/Test_2_2",
+                    "directorio_output":'/share_data/output/' + str(idFire) + "_" + str(fecha) + 'TEST',
                     "obj_incendio":"/share_data/input/ob_incendio/ob_180416.json",
                     "obj_perimetro": "/share_data/input/perimetros/per_180416.json",
-                    "service_account":"auth-algoritmos-bio@algoritmos-bio.iam.gserviceaccount.com",
+                    "service_account": Variable.get("dNBR_path_serviceAccount", default_var=None), 
                     "credenciales": "/share_data/input/algoritmos-bio-b40e24394020.json",
-                    "dias_pre":10,
-                    "dias_post":10
+                    "dias_pre":int(Variable.get("dNBR_diasPre", default_var=10)),
+                    "dias_post":int(Variable.get("dNBR_diasPost", default_var=10)),
                 }
                 print(params)
 
