3a626b2862c470e0ada6f822928b39dcd4cf541d
SaraAdr <sara.arrdr@gmail.com>
2025-02-14 11:34:34 +0100

    test2 sin combustible

--- algorithm_dNBR_processType1.py
+++ algorithm_dNBR_processType1.py
@@ -90,9 +90,9 @@ def ejecutar_algoritmo(datos, fechaHoraActual):
 
                 params = {          
                     "directorio_alg":".",
-                    "directorio_output":'/share_data/output/' + str(idFire) + "_" + str(fecha) + 'TEST',
-                    "obj_incendio":"/share_data/input/ob_incendio/ob_180416.json",
-                    "obj_perimetro": "/share_data/input/perimetros/per_180416.json",
+                    "directorio_output":'/share_data/output/' + str(idFire) + "_" + str(fecha) + 'TEST2',
+                    "obj_incendio":f'/share_data/input/ob_incendio/incendio_{idFire}_{fecha}.json',
+                    "obj_perimetro":  f'/share_data/input/perimetros/perimetro_{idFire}_{fecha}.json',
                     "service_account": Variable.get("dNBR_path_serviceAccount", default_var=None), 
                     "credenciales": "/share_data/input/algoritmos-bio-b40e24394020.json",
                     "dias_pre":int(Variable.get("dNBR_diasPre", default_var=10)),
