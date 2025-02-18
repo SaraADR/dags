5d291ee22aedeb86016c2e68e702ac50a83db5c6
SaraAdr <sara.arrdr@gmail.com>
2025-02-18 11:48:10 +0100

    dags util nuevo metodo

--- dag_utils.py
+++ dag_utils.py
@@ -217,6 +217,18 @@ def get_minio_client():
         config=Config(signature_version='s3v4')
     )
 
+def print_directory_contents(directory):
+    print(f"Contenido del directorio: {directory}")
+    for root, dirs, files in os.walk(directory):
+        level = root.replace(directory, '').count(os.sep)
+        indent = ' ' * 4 * level
+        print(f"{indent}{os.path.basename(root)}/")
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print(f"{subindent}{f}")
+    print("------------------------------------------")    
+
+
 
 def duration_to_seconds(duration: str) -> int:
     """
