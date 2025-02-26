8b93ea1f81895a081caeaf95abe983f80b150a3a
SaraAdr <sara.arrdr@gmail.com>
2025-02-26 16:34:52 +0100

    dnbr 2

--- algorithm_dNBR_processType2.py
+++ algorithm_dNBR_processType2.py
@@ -92,106 +92,104 @@ def ejecutar_algoritmo(datos, fechaHoraActual):
             print(f"Cambiando al directorio de lanzamiento y ejecutando limpieza de volumenes")
             stdin, stdout, stderr = ssh_client.exec_command('cd /home/admin3/algoritmo_dNBR/launch && docker-compose down --volumes')
             stdout.channel.recv_exit_status()  # Esperar a que el comando termine
-
-            for dato in datos:
-                print(f"dato: {dato}")
-                json_Incendio = busqueda_datos_incendio(dato)
-                json_Perimetro = busqueda_datos_perimetro(dato)
-
-                print(json_Incendio)
-                print(json_Perimetro)
-
-                fire_id, mission_id = datos
-
-                if json_Incendio is not None:
-                    archivo_incendio = f"/home/admin3/algoritmo_dNBR/input/ob_incendio/incendio_{fire_id}_{fecha}.json"
-                    ssh_client.exec_command(f"touch {archivo_incendio}")
-                    ssh_client.exec_command(f"chmod 644 {archivo_incendio}")
-                    with sftp.file(archivo_incendio, 'w') as json_file:
-                        json.dump(json_Incendio, json_file, ensure_ascii=False, indent=4)
-
-                if json_Perimetro is not None:                                    
-                    archivo_perimetro = f"/home/admin3/algoritmo_dNBR/input/perimetros/perimetro_{fire_id}_{fecha}.json"
-                    ssh_client.exec_command(f"touch {archivo_perimetro}")
-                    ssh_client.exec_command(f"chmod 644 {archivo_perimetro}")
-                    with sftp.file(archivo_perimetro, 'w') as json_file:
-                        json_file.write(json.dumps(json_Perimetro, indent=4).encode('utf-8'))
-                              
-
-                params = {
-                    "directorio_alg":  '.',
-                    "directorio_output" : '/share_data/output/' + str(fire_id) + "_" + str(fecha),
-                    "obj_incendio":  f'/share_data/input/ob_incendio/incendio_{fire_id}_{fecha}.json',
-                    "obj_perimetro":  f'/share_data/input/perimetros/perimetro_{fire_id}_{fecha}.json',
-                    "service_account" : Variable.get("dNBR_path_serviceAccount", default_var=None), 
-                    "credenciales" : '/share_data/input/algoritmos-bio-b40e24394020.json',
-                    "dias_pre" :  int(Variable.get("dNBR_diasPre", default_var=10)),
-                    "dias_post" : int(Variable.get("dNBR_diasPost", default_var=10)),
-                    "dia_fin": (datetime.datetime.now() - datetime.timedelta(days=10)).strftime("%Y-%m-%d"),
-                    "combustibles" : Variable.get("dNBR_pathCombustible", default_var="/share_data/input/galicia_mod_com_filt.tif") 
-                }
-                print(params)
-                output_data = {}
-                if params is not None:                                    
-                    archivo_params = f"/home/admin3/algoritmo_dNBR/input/ejecucion_{fire_id}_{fecha}.json"
-                    with sftp.file(archivo_params, 'w') as json_file:
-                        json.dump(params, json_file, ensure_ascii=False, indent=4)
-                        print(f"Guardado archivo {archivo_params}")
-
-                    path = f'/share_data/input/ejecucion_{fire_id}_{fecha}.json' 
-                    runId = f'{fire_id}_{fecha}'
-                    stdin, stdout, stderr = ssh_client.exec_command(
-                        f'cd /home/admin3/algoritmo_dNBR/scripts && '
-                        f'export CONFIGURATION_PATH={path} && '
-                        f'docker-compose -f ../launch/compose.yaml up --build && '
-                        f'docker-compose -f ../launch/compose.yaml down --volumes'
-                    )
-                    output = stdout.read().decode()
-                    error_output = stderr.read().decode()
-                    output_data = {}
-                    print("Salida de run.sh:")
-                    print(output)
-                    for line in output.split("\n"):
-                        if "Valor -3: La región del incendio no se incluye en la capa de combustibles." in line or "Valor -1: No se pudo generar una imagen" in line or "Valor -100" in line: 
-                            algorithm_error_message = line.strip()
-                            print(f"Error durante el guardado de la misión: {algorithm_error_message}")
-                            output_data = {"estado": "ERROR", "comentario": algorithm_error_message}
-                            historizacion(output_data, fire_id, mission_id )
-                            raise Exception(algorithm_error_message)
-                            
-                        
-                    # raise Exception(f"Error en la ejecución del script remoto: {error_output}")
-                    output_directory = f'/home/admin3/algoritmo_dNBR/output/' + str(fire_id) + "_" + str(fecha) 
-                    local_output_directory = f'/tmp'
-                    sftp.chdir(output_directory)
-                    print(f"Cambiando al directorio de salida: {output_directory}")
-                    downloaded_files = []
-                    for filename in sftp.listdir():
-                            remote_file_path = os.path.join(output_directory, filename)
-                            local_file_path = os.path.join(local_output_directory, filename)
-
-                            # Descargar el archivo
-                            sftp.get(remote_file_path, local_file_path)
-                            print(f"Archivo {filename} descargado a {local_file_path}")
-                            downloaded_files.append(local_file_path)
+            fire_id, mission_id = datos
             
-                    sftp.close()
-                    print_directory_contents(local_output_directory)
-                    local_output_directory = f'/tmp'
-                    archivos_en_tmp = os.listdir(local_output_directory)
+            print(f"dato: {datos}")
+            json_Incendio = busqueda_datos_incendio(fire_id)
+            json_Perimetro = busqueda_datos_perimetro(fire_id)
+
+            print(json_Incendio)
+            print(json_Perimetro)
+
+            if json_Incendio is not None:
+                archivo_incendio = f"/home/admin3/algoritmo_dNBR/input/ob_incendio/incendio_{fire_id}_{fecha}.json"
+                ssh_client.exec_command(f"touch {archivo_incendio}")
+                ssh_client.exec_command(f"chmod 644 {archivo_incendio}")
+                with sftp.file(archivo_incendio, 'w') as json_file:
+                    json.dump(json_Incendio, json_file, ensure_ascii=False, indent=4)
+
+            if json_Perimetro is not None:                                    
+                archivo_perimetro = f"/home/admin3/algoritmo_dNBR/input/perimetros/perimetro_{fire_id}_{fecha}.json"
+                ssh_client.exec_command(f"touch {archivo_perimetro}")
+                ssh_client.exec_command(f"chmod 644 {archivo_perimetro}")
+                with sftp.file(archivo_perimetro, 'w') as json_file:
+                    json_file.write(json.dumps(json_Perimetro, indent=4).encode('utf-8'))
+                            
+
+            params = {
+                "directorio_alg":  '.',
+                "directorio_output" : '/share_data/output/' + str(fire_id) + "_" + str(fecha),
+                "obj_incendio":  f'/share_data/input/ob_incendio/incendio_{fire_id}_{fecha}.json',
+                "obj_perimetro":  f'/share_data/input/perimetros/perimetro_{fire_id}_{fecha}.json',
+                "service_account" : Variable.get("dNBR_path_serviceAccount", default_var=None), 
+                "credenciales" : '/share_data/input/algoritmos-bio-b40e24394020.json',
+                "dias_pre" :  int(Variable.get("dNBR_diasPre", default_var=10)),
+                "dias_post" : int(Variable.get("dNBR_diasPost", default_var=10)),
+                "dia_fin": (datetime.datetime.now() - datetime.timedelta(days=10)).strftime("%Y-%m-%d"),
+                "combustibles" : Variable.get("dNBR_pathCombustible", default_var="/share_data/input/galicia_mod_com_filt.tif") 
+            }
+            print(params)
+            output_data = {}
+            if params is not None:                                    
+                archivo_params = f"/home/admin3/algoritmo_dNBR/input/ejecucion_{fire_id}_{fecha}.json"
+                with sftp.file(archivo_params, 'w') as json_file:
+                    json.dump(params, json_file, ensure_ascii=False, indent=4)
+                    print(f"Guardado archivo {archivo_params}")
+
+                path = f'/share_data/input/ejecucion_{fire_id}_{fecha}.json' 
+                runId = f'{fire_id}_{fecha}'
+                stdin, stdout, stderr = ssh_client.exec_command(
+                    f'cd /home/admin3/algoritmo_dNBR/scripts && '
+                    f'export CONFIGURATION_PATH={path} && '
+                    f'docker-compose -f ../launch/compose.yaml up --build && '
+                    f'docker-compose -f ../launch/compose.yaml down --volumes'
+                )
+                output = stdout.read().decode()
+                error_output = stderr.read().decode()
+                output_data = {}
+                print("Salida de run.sh:")
+                print(output)
+                for line in output.split("\n"):
+                    if "Valor -3: La región del incendio no se incluye en la capa de combustibles." in line or "Valor -1: No se pudo generar una imagen" in line or "Valor -100" in line: 
+                        algorithm_error_message = line.strip()
+                        print(f"Error durante el guardado de la misión: {algorithm_error_message}")
+                        output_data = {"estado": "ERROR", "comentario": algorithm_error_message}
+                        historizacion(output_data, fire_id, mission_id )
+                        raise Exception(algorithm_error_message)
+                        
                     
-                    key = uuid.uuid4()
-                    for archivo in archivos_en_tmp:
-                        archivo_path = os.path.join(local_output_directory, archivo)
-                        if not os.path.isfile(archivo_path):
-                            print(f"Skipping upload: {local_file_path} is not a file.")
-                        else:
-                            print(archivo)
-                            local_file_path = f"{mission_id}/{str(key)}"
-                            upload_to_minio_path('minio_conn', 'missions', local_file_path, archivo_path)
-                            output_data[archivo] = local_file_path
-                    output_data["estado"] = "FINISHED"
-                    historizacion(output_data, fire_id, mission_id, )
+                # raise Exception(f"Error en la ejecución del script remoto: {error_output}")
+                output_directory = f'/home/admin3/algoritmo_dNBR/output/' + str(fire_id) + "_" + str(fecha) 
+                local_output_directory = f'/tmp'
+                sftp.chdir(output_directory)
+                print(f"Cambiando al directorio de salida: {output_directory}")
+                downloaded_files = []
+                for filename in sftp.listdir():
+                        remote_file_path = os.path.join(output_directory, filename)
+                        local_file_path = os.path.join(local_output_directory, filename)
+
+                        # Descargar el archivo
+                        sftp.get(remote_file_path, local_file_path)
+                        print(f"Archivo {filename} descargado a {local_file_path}")
+                        downloaded_files.append(local_file_path)
+        
+                sftp.close()
+                print_directory_contents(local_output_directory)
+                local_output_directory = f'/tmp'
+                archivos_en_tmp = os.listdir(local_output_directory)
+                
+                key = uuid.uuid4()
+                for archivo in archivos_en_tmp:
+                    archivo_path = os.path.join(local_output_directory, archivo)
+                    if not os.path.isfile(archivo_path):
+                        print(f"Skipping upload: {local_file_path} is not a file.")
+                    else:
+                        print(archivo)
+                        local_file_path = f"{mission_id}/{str(key)}"
+                        upload_to_minio_path('minio_conn', 'missions', local_file_path, archivo_path)
+                        output_data[archivo] = local_file_path
+                output_data["estado"] = "FINISHED"
+                historizacion(output_data, fire_id, mission_id )
     except Exception as e:
         print(f"Error en el proceso: {str(e)}")    
         output_data = {"estado": "ERROR", "comentario": str(e)}
