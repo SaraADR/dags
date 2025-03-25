979f66494ef98f5cd08bcab240a12ab458e0a7ba
Óscar <oscar.lopez@cuatrodigital.com>
2025-03-25 08:34:41 +0100

    Actualizar subida y conexion a geoserver

--- algorithm_risk_map.py
+++ algorithm_risk_map.py
@@ -7,6 +7,11 @@ from airflow.providers.ssh.hooks.ssh import SSHHook
 import pytz
 from dag_utils import execute_query
 import time
+import requests
+from requests.auth import HTTPBasicAuth
+import os
+
+
 def execute_docker_process(**context):
     ssh_hook = SSHHook(ssh_conn_id="my_ssh_conn")
 
@@ -134,8 +139,61 @@ def store_in_db(**context):
         print(f"Error al guardar en la base de datos: {str(e)}")
 
 
+
+GEOSERVER_URL = "https://geoserver.dev.cuatrodigital.com/geoserver/rest"
+GEOSERVER_USER = "admin"
+GEOSERVER_PASSWORD = "geoserver"
+WORKSPACE = "Modelos_Combustible_2024"
+STORE = "riskmaps"
+
 def publish_to_geoserver(**context):
-    print("Publicando en Geoserver...")
+    tiff_files = context['task_instance'].xcom_pull(task_ids='check_output_files', key='output_files')
+    if not tiff_files:
+        raise Exception("No se encontraron archivos para publicar.")
+
+    for tiff_path in tiff_files:
+        layer_name = f"galicia_mapa_riesgo_{datetime.now().strftime('%Y%m%d_%H%M')}"
+        file_name = os.path.basename(tiff_path)
+
+        # 1. Publicar como nueva capa
+        url = f"{GEOSERVER_URL}/rest/workspaces/{WORKSPACE}/coveragestores/{layer_name}/file.geotiff"
+        headers = {"Content-type": "image/tiff"}
+
+        with SSHHook(ssh_conn_id="my_ssh_conn").get_conn() as ssh_client:
+            sftp = ssh_client.open_sftp()
+            with sftp.file(tiff_path, 'rb') as remote_file:
+                file_data = remote_file.read()
+
+        response = requests.put(
+            url,
+            headers=headers,
+            data=file_data,
+            auth=HTTPBasicAuth(GEOSERVER_USER, GEOSERVER_PASSWORD),
+            params={"configure": "all"}
+        )
+
+        if response.status_code not in [201, 202]:
+            raise Exception(f"Error al publicar {layer_name} en GeoServer: {response.text}")
+
+        print(f"Capa publicada: {layer_name}")
+
+        # 2. Actualizar capa genérica
+        generic_store = "galicia_mapa_riesgo_latest"
+        url_latest = f"{GEOSERVER_URL}/rest/workspaces/{WORKSPACE}/coveragestores/{generic_store}/file.geotiff"
+
+        response_latest = requests.put(
+            url_latest,
+            headers=headers,
+            data=file_data,
+            auth=HTTPBasicAuth(GEOSERVER_USER, GEOSERVER_PASSWORD),
+            params={"configure": "all"}
+        )
+
+        if response_latest.status_code not in [201, 202]:
+            raise Exception(f"Error al actualizar capa genérica: {response_latest.text}")
+
+        print(f"Capa genérica actualizada: galicia_mapa_riesgo_latest")
+
 
 
 default_args = {
