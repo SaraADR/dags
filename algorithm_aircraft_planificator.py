fc760132e7c1644eb94527ce06eae5898cf39e76
Óscar <oscar.lopez@cuatrodigital.com>
2025-04-02 12:58:27 +0200

    Update subida

--- algorithm_aircraft_planificator.py
+++ algorithm_aircraft_planificator.py
@@ -60,7 +60,7 @@ def execute_algorithm_remote(**context):
         # Ejecutar el algoritmo remoto
         # cmd = 'cd ~/algoritmo && source venv/bin/activate && python call_recomendador.py input/input_data_aeronaves.txt'
         # cmd = 'for d in /home /opt /srv /data /var /usr /root; do echo "Contenido de: $d" && ls -la $d; echo ""; done'
-        cmd = 'ls -l /home/citmaga/algoritmo_recomendador_objetivo5/call_recomendador.py'
+        cmd = 'ls -l /home/citmaga/algoritmo_recomendador_objetivo5'
 
 
         print(f"Ejecutando comando remoto:\n{cmd}")
