3e047a409285a2db2e026c67478aac8ee870bad0
SaraAdr <sara.arrdr@gmail.com>
2025-04-07 09:31:47 +0200

    logs desde control

--- read_upload_logs.py
+++ read_upload_logs.py
@@ -1,13 +1,39 @@
+import boto3
+import os
+from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
-from airflow import DAG
 from datetime import datetime
 from airflow.utils.trigger_rule import TriggerRule
+from airflow.models import TaskInstance
+from airflow.utils.db import provide_session
 
 # Función que imprime un mensaje cuando un DAG termina
-def print_message(**kwargs):
+def print_message(session, **kwargs):
     dag_name = kwargs['dag_run'].conf.get('dag_name', 'Desconocido')
-    print(f"DAG {dag_name} TERMINADO")
+    print(f"Ha finalizado este DAG: {dag_name}")
+    ti = session.query(TaskInstance).filter(TaskInstance.dag_id == dag_name).order_by(TaskInstance.execution_date.desc()).first()
+    if not ti:
+        log_path = "No se encontró una ejecución válida"
+    else:
+        log_path = f"/opt/airflow/logs/dag_id={dag_name}/run_id={ti.run_id}/task_id={ti.task_id}/attempt={ti.try_number}.log"
+
+    print(f"🔍 Se va a buscar este log: {log_path}")
+    return log_path
+
+
+def upload_to_minio(**kwargs):
+    log_path = kwargs['ti'].xcom_pull(task_ids='find_log')
+    
+    if not log_path or log_path.startswith("No se encontró"):
+        print("🚨 No se puede subir el log a MinIO, no existe un archivo válido.")
+        return
+    
+    # Leer el contenido del log
+    with open(log_path, "r") as log_file:
+        log_data = log_file.read()
+
+    print(log_data)
 
 default_args = {
     'owner': 'airflow',
@@ -18,7 +44,7 @@ default_args = {
 dag = DAG(
     'monitor_dags',
     default_args=default_args,
-    description='Monitorea otros DAGs y muestra un mensaje cuando terminan',
+    description='Monitorea otros DAGs, busca sus logs y los sube a MinIO',
     schedule_interval=None,
 )
 
@@ -29,5 +55,11 @@ print_msg_task = PythonOperator(
     dag=dag,
 )
 
+upload_to_minio_task = PythonOperator(
+    task_id='upload_to_minio',
+    python_callable=upload_to_minio,
+    provide_context=True,
+    dag=dag,
+)
 
-print_msg_task
+print_msg_task >> upload_to_minio_task
