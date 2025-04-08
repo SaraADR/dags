48377a5f67d3400bc9d485464e55122379c786aa
Óscar <oscar.lopez@cuatrodigital.com>
2025-04-08 11:38:55 +0200

    Update

--- algorithm_aircraft_planificator_oscar.py
+++ algorithm_aircraft_planificator_oscar.py
@@ -12,6 +12,7 @@ from airflow.hooks.base import BaseHook
 def execute_algorithm_remote(**context):
     message = context['dag_run'].conf
     input_data_str = message['message']['input_data']
+    
     if isinstance(input_data_str, str):
         input_data = json.loads(input_data_str)
     else:
@@ -51,16 +52,23 @@ def execute_algorithm_remote(**context):
 
         sftp = target_client.open_sftp()
 
-        # Input fijo esperado por el algoritmo
+        # Generar el fichero .properties para aeronaves
         remote_input_path = '/algoritms/algoritmo-recomendador-objetivo-5/Input/input_data_aeronaves.txt'
-
-        # GENERACIÓN DEL CONTENIDO .TXT A PARTIR DE input_data
-        missions = input_data.get('missionData', [])
-        lines = []
-        for m in missions:
-            line = f"{m['missionID']},{m['lonLat'][0]},{m['lonLat'][1]},{m['startDate']},{m['endDate']},{m['timeInterval']}"
-            lines.append(line)
-        txt_content = "\n".join(lines)
+        
+        properties_lines = [
+            "medios=a",
+            f"url1={input_data['url1']}",
+            f"url2={input_data['url2']}",
+            f"url3={input_data['url3']}",
+            f"url4={input_data['url4']}",
+            f"url5={input_data['url5']}",
+            f"url6={input_data['url6']}",
+            f"user={input_data['user']}",
+            f"password={input_data['password']}",
+            "modelos_aeronave=input/modelos_vehiculo.csv"
+        ]
+        
+        txt_content = "\n".join(properties_lines)
 
         with sftp.file(remote_input_path, 'w') as remote_file:
             remote_file.write(txt_content)
@@ -95,7 +103,7 @@ default_args = {
 dag = DAG(
     'algorithm_aircraft_planificator',
     default_args=default_args,
-    description='Ejecuta algoritmo de planificación en servidor Avincis',
+    description='Ejecuta algoritmo de planificación de aeronaves en servidor Avincis',
     schedule_interval=None,
     catchup=False,
     max_active_runs=1,
