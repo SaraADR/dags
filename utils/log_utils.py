8b91e9b464391c65c3626f956e762f3df5b5c627
Ángel Martínez <angel.martinez@cuatrodigital.com>
2025-04-12 16:29:25 +0200

    revert log utils

--- utils/log_utils.py
+++ utils/log_utils.py
@@ -1,18 +1,27 @@
-from airflow.operators.python import BranchPythonOperator, PythonOperator
-from airflow.operators.empty import EmptyOperator  # ← Asegúrate de importar esto
+from airflow.operators.python import ShortCircuitOperator, PythonOperator
+from function_save_logs_to_minio import save_logs_to_minio
 
-def setup_conditional_log_saving(dag, task_id='save_logs_to_minio', condition_function=None):
+def setup_conditional_log_saving(dag, 
+                                  task_id='save_logs_to_minio',
+                                  condition_function=None):
     """
-    Set conditional logs saving.
-    """
-    def _branch(**context):
-        if condition_function(**context):
-            return task_id  # Ejecutar save_logs_to_minio
-        return "skip_save_logs"  # Saltar (no hacer nada)
+    Set tasks to save logs only under condition.
 
-    check_activity = BranchPythonOperator(
+    Args:
+        dag: DAG in which the tasks are registered.
+        task_id: task name of saving logs task.
+        condition_function: it returns True if logs have to be saved.
+
+    Returns:
+        tuple: (check_task, save_task)
+    """
+
+    if not callable(condition_function):
+        raise ValueError("Se requiere una función 'condition_function' válida.")
+
+    check_activity = ShortCircuitOperator(
         task_id='check_activity_for_logs',
-        python_callable=_branch,
+        python_callable=condition_function,
         provide_context=True,
         dag=dag,
     )
@@ -24,10 +33,6 @@ def setup_conditional_log_saving(dag, task_id='save_logs_to_minio', condition_fu
         dag=dag,
     )
 
-    skip_task = EmptyOperator(  # ← Task "dummy" para la rama de salto
-        task_id="skip_save_logs",
-        dag=dag,
-    )
+    check_activity >> save_logs_task
 
-    check_activity >> [save_logs_task, skip_task]  # ← Conexión explícita de ramas
     return check_activity, save_logs_task
