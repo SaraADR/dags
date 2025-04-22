2d7f4b20d49425791f255badb5d6ac688a1c9917
SaraAdr <sara.arrdr@gmail.com>
2025-04-22 19:51:23 +0200

    path

--- water_analysis.py
+++ water_analysis.py
@@ -766,7 +766,7 @@ def upload_tiff_attachment(resource_ids, metadata_input, archivos):
 
                     thumbnail_api = f"{base_url}/records/{uuid}/thumbnail"
                     payload = {
-                        "url": f"attachments/{archivo_file_name}"  # asegúrate que sea sólo el path relativo
+                        "url": f"attachments/{archivo_file_name}" 
                     }
 
                     headers = {
