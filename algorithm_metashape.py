b227d2832f847000fbc4c43d465d0fa9237607b6
Miguel Sanchez <miguel.sanchez@cuatrodigital.com>
2025-05-20 10:22:09 +0200

    Fix internal error invalid memory alloc request size

--- algorithm_metashape.py
+++ algorithm_metashape.py
@@ -1,16 +1,104 @@
+import base64
+import os
+import tempfile
+import zipfile
+
 import pytz
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.python_operator import PythonOperator
-from dag_utils import get_minio_client
+from dag_utils import get_minio_client, execute_query
 from airflow.models import Variable
+from kafka_consumer_classify_files_and_trigger_dags import download_from_minio
 import uuid
 import io
 import json
 
 
 def process_json(**kwargs):
+
+    otros = kwargs['dag_run'].conf.get('otros', [])
     json_content_original = kwargs['dag_run'].conf.get('json')
+
+    trace_id = kwargs['dag_run'].conf['trace_id']
+    print(f"Processing with trace_id: {trace_id}")
+
+    if not json_content_original:
+        print("Ha habido un error con el traspaso de los documentos")
+        return
+    print("Archivos para procesar preparados")
+
+    s3_client = get_minio_client()
+    archivos = []
+    file_path_in_minio =  otros.replace('tmp/','')
+    folder_prefix = 'sftp/'
+    try:
+        local_zip_path = download_from_minio(s3_client, 'tmp', file_path_in_minio, 'tmp', folder_prefix)
+        print(local_zip_path)
+    except Exception as e:
+        print(f"Error al descargar desde MinIO: {e}")
+        raise
+
+    if local_zip_path is None:
+        print(f"No se pudo descargar el archivo desde MinIO: {local_zip_path}")
+        return
+
+    try:
+        if not os.path.exists(local_zip_path):
+            print(f"Archivo no encontrado: {local_zip_path}")
+            return
+
+
+        # Abre y procesa el archivo ZIP desde el sistema de archivos
+        with zipfile.ZipFile(local_zip_path, 'r') as zip_file:
+            zip_file.testzip()
+            print("El archivo ZIP es válido.")
+    except zipfile.BadZipFile:
+        print("El archivo no es un ZIP válido antes del procesamiento.")
+        return
+
+    try:
+        with zipfile.ZipFile(local_zip_path, 'r') as zip_file:
+            with tempfile.TemporaryDirectory() as temp_dir:
+                print(f"Directorio temporal creado: {temp_dir}")
+                zip_file.extractall(temp_dir)
+                file_list = zip_file.namelist()
+                print("Archivos en el ZIP:", file_list)
+                archivos = []
+                folder_structure = {}
+                for file_name in file_list:
+                    file_path = os.path.join(temp_dir, file_name)
+
+                    if os.path.isdir(file_path):
+                        # Si es un directorio, saltamos
+                        continue
+
+                    print(f"Procesando archivo: {file_name}")
+                    with open(file_path, 'rb') as f:
+                        content = f.read()
+
+                    directory = os.path.dirname(file_name)
+                    if directory not in folder_structure:
+                        folder_structure[directory] = []
+                    folder_structure[directory].append(file_name)
+                    if os.path.basename(file_name).lower() == 'algorithm_result.json':
+                        continue
+                    else:
+                        if file_name.lower().endswith('.las') and "cloud-" in file_name:
+                            empty_content = b''
+                            encoded_empty_content = base64.b64encode(empty_content).decode('utf-8')
+                            archivos.append({'file_name': file_name, 'content': encoded_empty_content})
+                            print(f"Archivo .las padre {file_name} procesado con 0 bytes")
+                        else:
+                            encoded_content = base64.b64encode(content).decode('utf-8')
+                            archivos.append({'file_name': file_name, 'content': encoded_content})
+                print("Estructura de carpetas y archivos en el ZIP:", folder_structure)
+
+    except zipfile.BadZipFile as e:
+        print(f"El archivo no es un ZIP válido: {e}")
+        return
+
+    # json_content_original = kwargs['dag_run'].conf.get('json')
 
     # Clonamos el JSON para no modificar el original
     updated_json = json.loads(json.dumps(json_content_original))  # deep copy
