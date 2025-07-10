88feeabf36ed02da0ed2642cf36695d86101b33b
Óscar <oscar.lopez@cuatrodigital.com>
2025-07-10 11:09:52 +0200

    Update

--- algorithm_flame_front.py
+++ algorithm_flame_front.py
@@ -1,8 +1,8 @@
+# -*- coding: utf-8 -*-
 import base64
 import os
 import tempfile
 import zipfile
-
 import pytz
 from airflow import DAG
 from datetime import datetime
@@ -14,6 +14,58 @@ import uuid
 import io
 import json
 from PIL import Image, UnidentifiedImageError
+import geopandas as gpd
+from shapely.geometry import Point, LineString
+
+
+def crear_y_subir_shapefiles(detections, image_name, temp_dir, s3_client, bucket_name, minio_base_path):
+    uuid_key = minio_base_path.split('/')[2]
+    mission_id = minio_base_path.split('/')[1]
+    shp_paths = []
+
+    print(f"[DEBUG] Generando SHP para imagen: {image_name}")
+
+    for tipo, elementos in detections.items():
+        if tipo not in ["Hotspot", "Frontflame"]:
+            continue
+
+        geometries = []
+        for item in elementos:
+            coords = item.get("world_coordinates", [])
+            if tipo == "Hotspot" and coords:
+                geometries.append(Point(coords))
+            elif tipo == "Frontflame" and coords:
+                geometries.append(LineString(coords))
+
+        if not geometries:
+            continue
+
+        gdf = gpd.GeoDataFrame(geometry=geometries, crs="EPSG:4326")
+
+        tipo_lower = tipo.lower()
+        base_filename = f"{tipo_lower}_{os.path.splitext(image_name)[0]}"
+        local_shp_dir = os.path.join(temp_dir, base_filename)
+        os.makedirs(local_shp_dir, exist_ok=True)
+
+        local_shp_path = os.path.join(local_shp_dir, base_filename + ".shp")
+        gdf.to_file(local_shp_path)
+
+        for ext in [".shp", ".shx", ".dbf", ".prj", ".cpg"]:
+            file_path = os.path.join(local_shp_dir, base_filename + ext)
+            if os.path.exists(file_path):
+                minio_key = f"{mission_id}/{uuid_key}/{base_filename}{ext}"
+                with open(file_path, 'rb') as file_data:
+                    s3_client.put_object(
+                        Bucket=bucket_name,
+                        Key=minio_key,
+                        Body=file_data,
+                        ContentType='application/octet-stream'
+                    )
+                full_path = f"{Variable.get('ruta_minIO').rstrip('/')}/{bucket_name}/{minio_key}"
+                shp_paths.append(full_path)
+                print(f"[DEBUG] SHP subido: {full_path}")
+
+    return shp_paths
 
 
 def process_json(**kwargs):
@@ -74,7 +126,6 @@ def process_json(**kwargs):
             print(json_key)
             ruta_minio = Variable.get("ruta_minIO")
 
-            # Procesar thumbnail en formato base64 y subirlo como imagen PNG a MinIO
             for resource in updated_json.get("executionResources", []):
                 for data_entry in resource.get("data", []):
                     thumb_ref = data_entry.get('value', {}).get('thumbnail', {}).get('thumbRef')
@@ -83,18 +134,12 @@ def process_json(**kwargs):
 
                     try:
                         print("[DEBUG] Procesando thumbnail base64 para subirlo como PNG...")
-
                         cleaned_base64 = thumb_ref.replace('\n', '').replace('\r', '')
-
-                        # Decodificar base64 a bytes
                         img_bytes = base64.b64decode(cleaned_base64)
-
-                        # Crear nombre del archivo PNG a partir del recurso principal
                         main_file_name = os.path.basename(resource['path'])
                         thumb_filename = os.path.splitext(main_file_name)[0] + "_thumbnail.png"
                         thumb_key = f"{id_mission}/{uuid_key}/{thumb_filename}"
 
-                        # Subir a MinIO
                         s3_client.put_object(
                             Bucket=bucket_name,
                             Key=thumb_key,
@@ -102,17 +147,12 @@ def process_json(**kwargs):
                             ContentType='image/png'
                         )
 
-                        # Actualizar la ruta en el JSON
                         thumb_path = f"{ruta_minio.rstrip('/')}/{bucket_name}/{thumb_key}"
                         data_entry['value']['thumbnail']['path'] = thumb_path
                         print(f"[DEBUG] Thumbnail subido y actualizado en JSON: {thumb_path}")
-
-                        # Opcional: eliminar los bytes base64
                         del data_entry['value']['thumbnail']['thumbRef']
-
                     except Exception as e:
                         print(f"[ERROR] Error procesando el thumbnail: {e}")
-
 
             file_lookup = {}
             for root, _, files in os.walk(temp_dir):
@@ -122,21 +162,17 @@ def process_json(**kwargs):
                     local_path = os.path.join(root, file_name)
                     file_lookup[file_name] = local_path
 
-            # Actualizar rutas en el JSON para dejar solo /missions/2054/uuid/nombre
             for resource in updated_json.get("executionResources", []):
                 old_path = resource['path']
                 file_name = os.path.basename(old_path)
-
                 if file_name not in file_lookup:
                     print(f"No se encontró {file_name} en el ZIP extraído.")
                     continue
-
                 new_path = f"/{bucket_name}/{id_mission}/{uuid_key}/{file_name}"
                 full_path = f"{ruta_minio.rstrip('/')}{new_path}"
                 resource['path'] = full_path
                 print(f"Ruta actualizada: {old_path} -> {full_path}")
 
-            # Subir archivos a MinIO directamente con el nombre base
             for file_name, local_path in file_lookup.items():
                 minio_key = f"{id_mission}/{uuid_key}/{file_name}"
                 try:
@@ -151,7 +187,29 @@ def process_json(**kwargs):
                 except Exception as e:
                     print(f"Error al subir {file_name} a MinIO: {e}")
 
-            # Subir el JSON actualizado
+            # CREAR SHAPEFILES
+            algorithm_json_path = os.path.join(temp_dir, 'algorithm_result.json')
+            if os.path.exists(algorithm_json_path):
+                with open(algorithm_json_path, 'r', encoding='utf-8') as f:
+                    detection_data = json.load(f)
+
+                detecciones_por_imagen = {}
+                for elemento in detection_data.get('detections', []):
+                    image_name = elemento.get('image_name')
+                    if image_name not in detecciones_por_imagen:
+                        detecciones_por_imagen[image_name] = {"Hotspot": [], "Frontflame": []}
+                    tipo = elemento.get('type')
+                    if tipo in ["Hotspot", "Frontflame"]:
+                        detecciones_por_imagen[image_name][tipo].append(elemento)
+
+                for resource in updated_json.get("executionResources", []):
+                    image_name = os.path.basename(resource['path'])
+                    detecciones = detecciones_por_imagen.get(image_name)
+                    if detecciones:
+                        rutas_shp = crear_y_subir_shapefiles(detecciones, image_name, temp_dir, s3_client, bucket_name, f"{bucket_name}/{id_mission}/{uuid_key}")
+                        for path_shp in rutas_shp:
+                            resource.setdefault("extraResources", []).append({"type": "shapefile", "path": path_shp})
+
             s3_client.put_object(
                 Bucket=bucket_name,
                 Key=json_key,
@@ -159,12 +217,9 @@ def process_json(**kwargs):
                 ContentType='application/json'
             )
             print(f'Archivo JSON actualizado subido correctamente a MinIO como {json_key}')
-
-            # Historizar
             historizacion(json_content_original, updated_json, id_mission, startTimeStamp, endTimeStamp)
 
 
-#HISTORIZACION
 def historizacion(input_data, output_data, mission_id, startTimeStamp, endTimeStamp):
     try:
         print("Guardamos en historización/flamefront")
@@ -198,7 +253,6 @@ def historizacion(input_data, output_data, mission_id, startTimeStamp, endTimeSt
         print(f"Error en el proceso: {str(e)}")
 
 
-# Definición del DAG
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -221,5 +275,4 @@ process_task = PythonOperator(
     dag=dag
 )
 
-# Definir el flujo de las tareas
 process_task
